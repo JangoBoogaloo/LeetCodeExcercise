8201a46eff9f5ead4a73cf01fe48076180b5b2c3
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-08-16 08:26:06 -0400

    code cleanup

--- LeetCode/BinaryTree/TreeNode.cs
+++ LeetCode/BinaryTree/TreeNode.cs
@@ -1,51 +1,50 @@
-﻿using System;
-namespace BinaryTree
-{
-	public class TreeNode
-	{
-		public int val;
-		public TreeNode? left;
-		public TreeNode? right;
-		public TreeNode(int val = 0, TreeNode? left=null, TreeNode? right=null)
-		{
-			this.val = val;
-			this.left = left;
-			this.right = right;
-		}
+﻿namespace BinaryTree;
 
-		public static TreeNode? BuildTree(IReadOnlyList<int?> values) {
-			if (values.Count == 0) return null;
+public class TreeNode
+{
+	public int val;
+	public TreeNode? left;
+	public TreeNode? right;
+	public TreeNode(int val = 0, TreeNode? left=null, TreeNode? right=null)
+	{
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+
+	public static TreeNode? BuildTree(IReadOnlyList<int?> values) {
+		if (values.Count == 0) return null;
             var root = new TreeNode((int)values[0]!);
-			var nodes = new Queue<TreeNode?>();
-			nodes.Enqueue(root);
-			var i = 1;
-			while (i<values.Count) {
+		var nodes = new Queue<TreeNode?>();
+		nodes.Enqueue(root);
+		var i = 1;
+		while (i<values.Count) {
                 var node = nodes.Dequeue();
-				if (node is null) {
-					i += 2;
-					continue; 
-				}
+			if (node is null) {
+				i += 2;
+				continue; 
+			}
                 node.left = values[i] is null ? null : new TreeNode((int)values[i]!);
                 nodes.Enqueue(node.left);
-				i++;
+			i++;
                 node.right = (i < values.Count && values[i] is not null) ? new TreeNode((int)values[i]!) : null;
                 nodes.Enqueue(node.right);
                 i++;
             }
-			return root;
-		}
+		return root;
+	}
 
-		public static IReadOnlyList<int?> ToList(TreeNode? root) {
+	public static IReadOnlyList<int?> ToList(TreeNode? root) {
             var result = new List<int?>();
             if (root is null) return result;
-			var nodes = new Queue<TreeNode?>();
-			nodes.Enqueue(root);
-			var nonNullExist = true;
-			while (nonNullExist) {
-				var nodeCount = nodes.Count;
-				nonNullExist = false;
+		var nodes = new Queue<TreeNode?>();
+		nodes.Enqueue(root);
+		var nonNullExist = true;
+		while (nonNullExist) {
+			var nodeCount = nodes.Count;
+			nonNullExist = false;
                 for (int i=0; i<nodeCount; i++)
-				{
+			{
                     var node = nodes.Dequeue();
                     if (node is null)
                     {
@@ -55,21 +54,19 @@ namespace BinaryTree
                     }
                     else
                     {
-						nonNullExist = true;
+					nonNullExist = true;
                         result.Add(node.val);
                         nodes.Enqueue(node.left);
                         nodes.Enqueue(node.right);
                     }
                 }
-			}
+		}
 
-			for (int i = result.Count - 1; i >= 0; i--) {
-				if (result[i] is not null) break;
-				result.RemoveAt(i);
-			}
+		for (int i = result.Count - 1; i >= 0; i--) {
+			if (result[i] is not null) break;
+			result.RemoveAt(i);
+		}
 
             return result;
         }
-	}
 }
-
--- LeetCode/BinaryTreeTests/TreeNodeTests.cs
+++ LeetCode/BinaryTreeTests/TreeNodeTests.cs
@@ -8,11 +8,14 @@ public class TreeNodeTests
     {
         Array.Empty<int?>(),
         new int?[] { 1, },
+        new int?[] { 1, 2 },
+        new int?[] { 1, null, 3 },
         new int?[] { 1, 2, 3, 4},
         new int?[] { 1, 2, null, 4},
         new int?[] { 1, 2, null, null, 5},
         new int?[] { 1, null, 3, null, null, 6, 7},
         new int?[] { 1, 2, 3, null, 5, 6, 7},
+        new int?[] { 1, 2, 3, null, null, 6, 7},
         new int?[] { 1, 2, 3, null, 5, null, 7},
     };
 
