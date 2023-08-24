853dc816e481627b5968a5d0b9b5b60d7e7542a7
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-08-24 09:42:48 -0400

    Recursive solution to flatten binary tree

---
+++ LeetCode/BinaryTreeTests/BinaryTreeQuestion_114.cs
@@ -0,0 +1,62 @@
+﻿using BinaryTree;
+
+namespace BinaryTreeTests;
+
+[TestFixture]
+class BinaryTreeQuestion_114
+{
+    private static IReadOnlyList<TestCase_114> _testCase114 = new[]
+    {
+        new TestCase_114
+        {
+            Input = new int?[] { 1, 2, 5, 3, 4, null, 6 },
+            Expected = new int?[]
+            {
+                1, null, 2,
+                null, null, null, 3,
+                null, null, null, null, null, null, null, 4,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 5,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 6
+            }
+        }
+    };
+
+    [TestCaseSource(nameof(_testCase114))]
+    public void TestQuestion_114_Solution1(TestCase_114 testCase114)
+    {
+        var solution = new Solution1();
+        var tree = TreeNode.BuildTree(testCase114.Input);
+        solution.Flatten(tree);
+        var actual = TreeNode.ToList(tree).ToArray();
+        CollectionAssert.AreEqual(testCase114.Expected, actual);
+    }
+
+    public class TestCase_114
+    {
+        public int?[] Input;
+        public int?[] Expected;
+    }
+
+    class Solution1
+    {
+        public void Flatten(TreeNode root)
+        {
+            FlattenHelp(root);
+
+            TreeNode FlattenHelp(TreeNode node) {
+                if(node is null) return null;
+                if(node.left is null && node.right is null) return node;
+                var leftEnd = FlattenHelp(node.left);
+                var rightEnd = FlattenHelp(node.right);
+                if(leftEnd is not null) {
+                    leftEnd.right = node.right;
+                    node.right = node.left;
+                    node.left = null;
+                }
+
+                return rightEnd is null ? leftEnd : rightEnd;
+            }
+        }
+    }
+}
--- LeetCode/BinaryTreeTests/TreeNodeTests.cs
+++ LeetCode/BinaryTreeTests/TreeNodeTests.cs
@@ -18,6 +18,17 @@ class TreeNodeTests
         new int?[] { 1, 2, 3, null, 5, 6, 7},
         new int?[] { 1, 2, 3, null, null, 6, 7},
         new int?[] { 1, 2, 3, null, 5, null, 7},
+        new int?[] {1, null, 2,
+                    null, null, null, 3, 
+                    null, null, null, null, null, null, null, 4,
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 5,
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 6,
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 
+                    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 7
+        }
     };
 
     [TestCaseSource(nameof(_binaryTreeTestCases))]
