05285421d582d0110ee69aeae189f35233246f5b
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-08-26 10:55:12 -0400

    Maximum Path Sum 124

---
+++ LeetCode/BinaryTreeTests/MaximumPathSum_124.cs
@@ -0,0 +1,58 @@
+﻿namespace BinaryTreeTests;
+
+public class MaximumPathSum_124
+{
+    class Solution
+    {
+        public int MaxPathSum(TreeNode root)
+        {
+            var maxSum = int.MinValue;
+            MaxPathSumHelp(root);
+            return maxSum;
+            int MaxPathSumHelp(TreeNode? node)
+            {
+                if (node is null)
+                {
+                    return 0;
+                }
+
+                var leftDepthSum = MaxPathSumHelp(node.left);
+                var rightDepthSum = MaxPathSumHelp(node.right);
+                var maxDepthSum = Math.Max(leftDepthSum, rightDepthSum) + node.val;
+                var treePathSum = leftDepthSum + node.val + rightDepthSum;
+                maxSum = Math.Max(treePathSum, maxSum);
+                return maxDepthSum;
+            }
+        }
+    }
+
+    private static IReadOnlyList<MaximumPathSum_124TestCase> _maximumPathSum124TestCases = new[]
+    {
+        new MaximumPathSum_124TestCase()
+        {
+            Input = new int?[] { -10, 9, 20, null, null, 15, 7 },
+            Expected = 42
+        },
+        new MaximumPathSum_124TestCase()
+        {
+            Input = new int?[] { 1,-2,-3,1,3,-2,null,-1 },
+            Expected = 3
+        }
+    };
+    
+    [TestCaseSource(nameof(_maximumPathSum124TestCases))]
+    public void TestMaximumPathSum_124(MaximumPathSum_124TestCase testCase)
+    {
+        var solution = new Solution();
+        var tree = TreeNode.BuildTree(testCase.Input);
+        var actual = solution.MaxPathSum(tree);
+        Assert.That(actual, Is.EqualTo(testCase.Expected));
+    }
+
+    public class MaximumPathSum_124TestCase
+    {
+        public int?[] Input;
+        public int Expected;
+    }
+}
+
