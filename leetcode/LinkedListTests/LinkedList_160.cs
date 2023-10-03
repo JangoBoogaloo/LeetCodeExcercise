478e9f9bad0715ebe49fad2f839729b6491b9ae1
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-10-03 08:46:02 -0400

    two pointers solution to find intersection

--- leetcode/LinkedListTests/LinkedList_160.cs
+++ leetcode/LinkedListTests/LinkedList_160.cs
@@ -25,6 +25,50 @@ class LinkedList_160
         }
     }
 
+    private class SolutionTwoPointers
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            var tailA = GetLast(headA);
+            tailA.next = headA;
+            var intersect = DetectCycle(headB);
+            tailA.next = null;
+            return intersect;
+            ListNode? GetLast(ListNode? head)
+            {
+                if (head is null) return null;
+                while (head is not null)
+                {
+                    if (head.next is null) return head;
+                    head = head.next;
+                }
+                return null;
+            }
+            
+            ListNode? DetectCycle(ListNode? head)
+            {
+                var slow = head;
+                var fast = head;
+                while (fast is not null && fast.next is not null)
+                {
+                    fast = fast.next.next;
+                    slow = slow!.next;
+                    if (slow == fast) break;
+                }
+
+                if (fast == null || fast.next == null) return null;
+                slow = head;
+                while (slow != fast)
+                {
+                    slow = slow!.next;
+                    fast = fast!.next;
+                }
+
+                return slow;
+            }
+        }
+    }
+
     public static IReadOnlyList<LinkedList_160TestCase> LinkedListTestCases = new[]
     {
         new LinkedList_160TestCase(
@@ -45,6 +89,14 @@ class LinkedList_160
     public void TestLinkedList_160_Hash(LinkedList_160TestCase testCase)
     {
         var solution = new SolutionHash();
+        var actual = solution.GetIntersectionNode(testCase.HeadA, testCase.headB);
+        Assert.That(actual, Is.EqualTo(testCase.Intersect));
+    }
+
+    [TestCaseSource(nameof(LinkedListTestCases))]
+    public void TestLinkedList_160_TwoPointers(LinkedList_160TestCase testCase)
+    {
+        var solution = new SolutionTwoPointers();
         var actual = solution.GetIntersectionNode(testCase.HeadA, testCase.headB);
         Assert.That(actual, Is.EqualTo(testCase.Intersect));
     }
