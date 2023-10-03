231baed30132da8eb309284aba75318b96e3c40a
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-10-03 10:19:24 -0400

    Remove Nth Node From End of List single pass

---
+++ leetcode/LinkedListTests/LinkedList_19.cs
@@ -0,0 +1,24 @@
+﻿namespace LinkedListTests;
+
+[TestFixture]
+class LinkedList_19
+{
+    class Solution {
+        public ListNode RemoveNthFromEnd(ListNode head, int n) {
+            var fast = head;
+            for(var i=0; i<n; i++) {
+                fast = fast.next;
+            }
+            var dummyHead = new ListNode(0);
+            var slow =dummyHead;
+            slow.next = head;
+            while(fast is not null) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            slow.next = slow.next.next;
+            return dummyHead.next;
+        }
+    }
+    
+}
