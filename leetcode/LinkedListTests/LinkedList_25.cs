ef38ee9d7037c853a075b8e17f13a8b95741b5a9
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-10-06 10:51:59 -0400

    recursive approach

---
+++ leetcode/LinkedListTests/LinkedList_25.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedListTests;
+
+[TestFixture]
+class LinkedList_25
+{
+    class SolutionRecursive {
+        public ListNode? ReverseKGroup(ListNode? head, int k) {
+            if(head is null) return head;
+            var end = head;
+            for(var i=0; i < k; i++) {
+                if(end == null) return head;
+                end = end.next;
+            }
+            var newHead = Reverse(head, end);
+            head.next = ReverseKGroup(end, k);
+            return newHead;
+        }
+
+        private static ListNode? Reverse(ListNode? begin, ListNode? end) {
+            ListNode? prev = null;
+            var next = begin;
+            while(begin !=end) {
+                next = begin.next;
+                begin.next = prev;
+                prev = begin;
+                begin = next;
+            }
+            return prev;
+        }
+    }
+}
+
