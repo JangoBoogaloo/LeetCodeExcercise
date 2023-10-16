5409e9330f95e8e7e09a544737f29caade8b1cf4
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-10-16 13:32:29 -0400

    O(1) memory solution

--- leetcode/LinkedListTests/LinkedList_138.cs
+++ leetcode/LinkedListTests/LinkedList_138.cs
@@ -30,4 +30,47 @@ class LinkedList_138
             }
         }
     }
+    
+    class Solution_BigO1_Memory {
+        public Node? CopyRandomList(Node? head) {
+            if(head is null) return null;
+            var curr = head;
+            while (curr is not null)
+            {
+                var newNode = new Node(curr.val)
+                {
+                    next = curr.next
+                };
+                curr.next = newNode;
+                curr = newNode.next;
+            }
+
+            curr = head;
+            while (curr is not null)
+            {
+                if (curr.random is not null)
+                {
+                    curr.next.random = curr.random.next;
+                }
+                curr = curr.next.next;
+            }
+
+            var oldHead = head;
+            var newHead = head.next;
+            var result = newHead;
+            while (oldHead is not null)
+            {
+                oldHead.next = oldHead.next.next;
+                if (newHead.next is not null)
+                {
+                    newHead.next = newHead.next.next;
+                }
+
+                oldHead = oldHead.next;
+                newHead = newHead.next;
+            }
+
+            return result;
+        }
+    }
 }
