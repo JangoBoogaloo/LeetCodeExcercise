1a0ba1c86ebb2ceb66b17b80ae658007fce4bc39
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-10-16 13:56:03 -0400

    browser history linkedlist approach

---
+++ leetcode/LinkedList/DoubleNode.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkedList;
+
+public class DoubleNode
+{
+    public string val;
+    public DoubleNode prev;
+    public DoubleNode next;
+    public DoubleNode(string val) {
+        this.val = val;
+    }
+}
