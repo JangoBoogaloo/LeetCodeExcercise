7ae43cb0d1d1be62c10c73eeeb0db0dfd109846c
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-11-03 11:39:56 -0400

    78. Subsets

---
+++ leetcode/BackTrack/BackTrack_78.cs
@@ -0,0 +1,24 @@
+﻿namespace BackTrack;
+
+[TestFixture]
+class BackTrack_78
+{
+    public class Solution {
+        public IList<IList<int>> Subsets(int[] nums) {
+            var result = new List<IList<int>>();
+            var aSet = new List<int>();
+            Backtrack(0);
+            return result;
+
+            void Backtrack(int start)
+            {
+                result.Add(new List<int>(aSet));
+                for(int i=start; i<nums.Length; i++) {
+                    aSet.Add(nums[i]);
+                    Backtrack(i+1);
+                    aSet.RemoveAt(aSet.Count-1);
+                }
+            }
+        }
+    }
+}
