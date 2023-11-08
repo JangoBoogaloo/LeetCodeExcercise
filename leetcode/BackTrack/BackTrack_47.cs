369d3d9109897e6a5e49cfc9e235905976ed12c3
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-11-08 09:31:03 -0500

    47. Permutations II

---
+++ leetcode/BackTrack/BackTrack_47.cs
@@ -0,0 +1,34 @@
+﻿namespace BackTrack;
+
+[TestFixture]
+class BackTrack_47
+{
+    class Solution {
+        public IList<IList<int>> PermuteUnique(int[] nums) {
+            Array.Sort(nums);
+            var permutations = new List<IList<int>>();
+            var aPermutation = new List<int>();
+            var used = new bool[nums.Length];
+            Backtrack();
+            return permutations;
+
+            void Backtrack()
+            {
+                if(aPermutation.Count == nums.Length) {
+                    permutations.Add(new List<int>(aPermutation));
+                    return;
+                }
+                for(var i=0; i<nums.Length; i++) {
+                    if(used[i]) continue;
+
+                    if(i>0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+                    used[i] = true;
+                    aPermutation.Add(nums[i]);
+                    Backtrack();
+                    used[i] = false;
+                    aPermutation.RemoveAt(aPermutation.Count-1);
+                }
+            }
+        }
+    }    
+}
