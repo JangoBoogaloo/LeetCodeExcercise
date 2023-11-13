d4fdcbb6638ca7fb12e020fe2376efecddad6393
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-11-13 10:17:03 -0500

    22. Generate Parentheses

---
+++ leetcode/BackTrack/BackTrack_22.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace BackTrack;
+
+[TestFixture]
+class BackTrack_22
+{
+    class Solution {
+        public IList<string> GenerateParenthesis(int n) {
+            var combinations = new List<string>();
+            var leftCount = 0;
+            var rightCount = 0;
+            var setLenght = n*2;
+            var aSet = new StringBuilder();
+            Backtrack();
+            return combinations;
+
+            void Backtrack() {
+                if(aSet.Length == setLenght) {
+                    combinations.Add(aSet.ToString());
+                    return;
+                }
+                if(leftCount < n) {
+                    aSet.Append('(');
+                    leftCount++;
+                    Backtrack();
+                    aSet.Length--;
+                    leftCount--;
+                }
+                if(leftCount > rightCount) {
+                    aSet.Append(')');
+                    rightCount++;
+                    Backtrack();
+                    aSet.Length--;
+                    rightCount--;
+                }
+            }
+        }
+    }
+}
