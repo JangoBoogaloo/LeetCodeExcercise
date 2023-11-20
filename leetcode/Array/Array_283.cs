c15ed26e57fa28d06a567567c03726e6c25db66f
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-11-20 09:51:56 -0500

    283. Move Zeroes

---
+++ leetcode/Array/Array_283.cs
@@ -0,0 +1,26 @@
+﻿namespace Array;
+
+[TestFixture]
+class Array_283
+{
+    class Solution {
+        public void MoveZeroes(int[] nums) {
+            var zeroStart = RemoveElement(nums, 0);
+            for(var i = zeroStart; i<nums.Length; i++) {
+                nums[i] = 0;
+            }
+        }
+
+        private int RemoveElement(int[] nums, int val) {
+            var slowIndex = 0;
+            for(var fastIndex=0; fastIndex<nums.Length; fastIndex++)
+            {
+                if(nums[fastIndex] != val) {
+                    nums[slowIndex] = nums[fastIndex];
+                    slowIndex++;
+                }
+            }
+            return slowIndex;
+        }
+    }
+}
