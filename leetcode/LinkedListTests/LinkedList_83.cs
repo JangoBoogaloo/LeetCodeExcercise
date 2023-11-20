22fd3db774bb93a8d652c4bdd4f2395c5c10c741
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-11-20 11:25:48 -0500

    83. Remove Duplicates from Sorted List

---
+++ leetcode/LinkedListTests/LinkedList_83.cs
@@ -0,0 +1,22 @@
+﻿namespace LinkedListTests;
+
+public class LinkedList_83
+{
+    class Solution {
+        public ListNode? DeleteDuplicates(ListNode? head) {
+            if(head is null) return head;
+
+            var slow = head;
+            var fast = head.next;
+            while(fast is not null) {
+                if(fast.val != slow.val) {
+                    slow.next = fast;
+                    slow = slow.next;
+                }
+                fast = fast.next;
+            }
+            slow.next = null;
+            return head;
+        }
+    }
+}
