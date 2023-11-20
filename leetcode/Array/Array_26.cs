51fe1869c11b2270ca74a8e8d2f48507684637bd
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2023-11-20 09:45:44 -0500

    26. Remove Duplicates from Sorted Array

---
+++ leetcode/Array/Array_26.cs
@@ -0,0 +1,20 @@
+﻿namespace Array;
+
+[TestFixture]
+class Array_26
+{
+    class Solution {
+        public int RemoveDuplicates(int[] nums) {
+            if(nums.Length ==0) return 0;
+            var slowIndex = 0;
+            for(var fastIndex=1; fastIndex<nums.Length; fastIndex++)
+            {
+                if(nums[fastIndex] != nums[fastIndex-1]) {
+                    slowIndex++;
+                    nums[slowIndex] = nums[fastIndex];
+                }
+            }
+            return slowIndex+1;
+        }
+    }
+}
