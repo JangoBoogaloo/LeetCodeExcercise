9b06a70ff673d5a6b2908f498919c05282ce38f9
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-08 13:09:14 -0500

    recursive solution

---
+++ leetcode/BinaryTreeTests/BinaryTree_144.cs
@@ -0,0 +1,20 @@
+﻿namespace BinaryTreeTests;
+
+[TestFixture]
+class BinaryTree_144
+{
+    class SolutionRecursive {
+        public IList<int> PreorderTraversal(TreeNode root) {
+            var result = new List<int>();
+            PreorderTraversalRecursive(root);
+            return result;
+
+            void PreorderTraversalRecursive(TreeNode root) {
+                if(root is null) return;
+                result.Add(root.val);
+                PreorderTraversalRecursive(root.left);
+                PreorderTraversalRecursive(root.right);
+            }
+        }
+    }
+}
