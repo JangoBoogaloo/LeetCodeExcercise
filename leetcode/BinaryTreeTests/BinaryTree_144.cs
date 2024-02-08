4c628b5fc82d3bd80dfe221578c687b0799ce0a1
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-08 13:25:58 -0500

    stack iterative approach

--- leetcode/BinaryTreeTests/BinaryTree_144.cs
+++ leetcode/BinaryTreeTests/BinaryTree_144.cs
@@ -17,4 +17,30 @@ class BinaryTree_144
             }
         }
     }
+    
+    class SolutionStackIterative {
+        public IList<int> PreorderTraversal(TreeNode root) {
+            var result = new List<int>();
+            var stack = new Stack<TreeNode>();
+            if (root is null) return result;
+
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                result.Add(node.val);
+                if (node.right is not null)
+                {
+                    stack.Push(node.right);
+                }
+
+                if (node.left is not null)
+                {
+                    stack.Push(node.left);
+                }
+            }
+
+            return result;
+        }
+    }
 }
