d2063f8635ee19067cb2d2fda7aaa7358de9a610
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-20 10:00:48 -0500

    103. Binary Tree Zigzag Level Order Traversal

---
+++ leetcode/BinaryTreeTests/BinaryTree_103.cs
@@ -0,0 +1,67 @@
+﻿namespace BinaryTreeTests;
+
+[TestFixture]
+internal class BinaryTree_103
+{
+    private class Solution {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root) {
+            var result = new List<IList<int>>();
+            if(root is null) return result;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var fromRight = false;
+            while(queue.Any()) {
+                var levelList = new List<int>();
+                var levelSize = queue.Count;
+                for(var i=0; i<levelSize; i++) {
+                    var node = queue.Dequeue();
+                    levelList.Add(node.val);
+                    if(node.left is not null) {
+                        queue.Enqueue(node.left);
+                    }
+                    if(node.right is not null) {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                if(fromRight) {
+                    levelList.Reverse();
+                }
+                result.Add(levelList);
+                //flip the level traversal order
+                fromRight = !fromRight;
+            }
+            return result;
+        }
+    }
+}
+
+class Solution {
+    public IList<IList<int>> ZigzagLevelOrder(TreeNode root) {
+        var result = new List<IList<int>>();
+        if(root is null) return result;
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        var fromRight = false;
+        while(queue.Any()) {
+            var levelList = new List<int>();
+            var levelSize = queue.Count;
+            for(var i=0; i<levelSize; i++) {
+                var node = queue.Dequeue();
+                levelList.Add(node.val);
+                if(node.left is not null) {
+                    queue.Enqueue(node.left);
+                }
+                if(node.right is not null) {
+                    queue.Enqueue(node.right);
+                }
+            }
+            if(fromRight) {
+                levelList.Reverse();
+            }
+            result.Add(levelList);
+            //flip the level traversal order
+            fromRight = !fromRight;
+        }
+        return result;
+    }
+}
