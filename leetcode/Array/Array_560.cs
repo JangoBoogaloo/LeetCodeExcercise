a6979b4afe5321500e4e938d86b395da22aee691
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-23 20:33:37 -0500

    560. Subarray Sum Equals K

---
+++ leetcode/Array/Array_560.cs
@@ -0,0 +1,25 @@
+﻿namespace Array;
+
+[TestFixture]
+internal class Array_560
+{
+    private class Solution {
+        public int SubarraySum(int[] nums, int target) {
+            var count = 0;
+            var currSum = 0;
+            var sumOccurance = new Dictionary<int, int>();
+            foreach(var num in nums)
+            {
+                currSum += num;
+                if(currSum == target) {
+                    count++;
+                }
+                var targetDiff = currSum - target;
+                count += sumOccurance.GetValueOrDefault(targetDiff, 0);
+                sumOccurance[currSum] = sumOccurance.GetValueOrDefault(currSum, 0) + 1;
+            }
+
+            return count;
+        }
+    }
+}
