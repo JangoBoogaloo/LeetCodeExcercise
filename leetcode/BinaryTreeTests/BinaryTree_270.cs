2700d44d88ad8371b730b7006cbb9aeeb61633d7
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-24 22:38:18 -0500

    270. Closest Binary Search Tree Value

---
+++ leetcode/BinaryTreeTests/BinaryTree_270.cs
@@ -0,0 +1,26 @@
+﻿namespace BinaryTreeTests;
+
+[TestFixture]
+internal class BinaryTree_270
+{
+    class Solution {
+        public int ClosestValue(TreeNode root, double target) {
+            var smallestDiff = double.MaxValue;
+            var result = int.MaxValue;
+            Dfs(root);
+            return result;
+
+            void Dfs(TreeNode root) {
+                if(root is null) return;
+                var diff = double.Abs(target - root.val);
+                if(diff < smallestDiff ||
+                   (diff == smallestDiff && result > root.val)) {
+                    result = root.val;
+                    smallestDiff = diff;
+                }
+                Dfs(root.left);
+                Dfs(root.right);
+            }
+        }
+    }
+}
