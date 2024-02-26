a837d969b3473ca017afcb2d5b58b370dbf0e510
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-26 17:18:18 -0500

    257. Binary Tree Paths

---
+++ leetcode/BinaryTreeTests/BinaryTree_257.cs
@@ -0,0 +1,24 @@
+﻿namespace BinaryTreeTests;
+
+[TestFixture]
+internal class BinaryTree_257
+{
+    private class Solution {
+        public IList<string> BinaryTreePaths(TreeNode root) {
+            List<string> result = new List<string>();
+            DFS(root, "", result);
+            return result;
+        }
+
+        private void DFS(TreeNode node, string path, List<string> result) {
+            if (node == null) return;
+            path += node.val.ToString();
+            if (node.left == null && node.right == null) {
+                result.Add(path);
+            } else {
+                DFS(node.left, path + "->", result);
+                DFS(node.right, path + "->", result);
+            }
+        }
+    }
+}
