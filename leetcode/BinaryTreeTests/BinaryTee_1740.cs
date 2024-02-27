51053d97e02e8c89ecf3c56672e67d994b37b9c9
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-27 13:21:12 -0500

    1740. Find Distance in a Binary Tree

---
+++ leetcode/BinaryTreeTests/BinaryTee_1740.cs
@@ -0,0 +1,48 @@
+﻿namespace BinaryTreeTests;
+
+[TestFixture]
+internal class BinaryTee_1740
+{
+    class Solution {
+        private int result;
+
+        public int FindDistance(TreeNode root, int p, int q) {
+            if(p == q) return 0;
+            result = -1;
+            DfsDistanceToOneOfThem(root, p, q);
+            return result;
+        }
+
+        private int DfsDistanceToOneOfThem(TreeNode root, int p, int q) {
+            if(root is null) return -1;
+            var leftDistance = DfsDistanceToOneOfThem(root.left, p, q);
+            var rightDistance = DfsDistanceToOneOfThem(root.right, p, q);
+
+            if(root.val == p || root.val == q) {
+                if (leftDistance < 0 && rightDistance < 0) {
+                    return 0;
+                }
+                result = 1 + (leftDistance >= 0 ? leftDistance : rightDistance);
+
+                //We already find result, set -1 for this branch to quick return
+                return -1;
+            }
+
+            //ROOT is LCA
+            if (leftDistance >= 0 && rightDistance >= 0) {
+                result = leftDistance + rightDistance + 2;
+                return -1;
+            }
+
+            if (leftDistance >= 0) {
+                return leftDistance + 1;
+            }
+            if (rightDistance >= 0) {
+                return rightDistance + 1;
+            }
+
+            //p and q are not found in this tree
+            return -1;
+        }
+    }
+}
