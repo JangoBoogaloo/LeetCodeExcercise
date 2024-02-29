e1a3906973cb55b3958524c7c3a8f2d165209fc1
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-02-29 09:19:46 -0500

    better comment

--- leetcode/BinaryTreeTests/BinaryTree_333.cs
+++ leetcode/BinaryTreeTests/BinaryTree_333.cs
@@ -9,18 +9,22 @@ internal class BinaryTree_333
         {
             public int MinValue;
             public int MaxValue;
-            public int LargestBstNodes;
+            
+            /// <summary>
+            /// the number of nodes for the largest binary search treee
+            /// </summary>
+            public int LargestBst;
         }
         
         public int LargestBSTSubtree(TreeNode root)
         {
             var treeInfo = PostOrder(root);
-            return treeInfo.LargestBstNodes;
+            return treeInfo.LargestBst;
         }
 
         private TreeInfo PostOrder(TreeNode? node)
         {
-            var treeInfo = new TreeInfo() { MinValue = int.MaxValue, MaxValue = int.MinValue, LargestBstNodes = 0 };
+            var treeInfo = new TreeInfo() { MinValue = int.MaxValue, MaxValue = int.MinValue, LargestBst = 0 };
             if (node is null)
             {
                 return treeInfo;
@@ -34,14 +38,14 @@ internal class BinaryTree_333
             {
                 treeInfo.MinValue = Math.Min(node.val, leftTreeInfo.MinValue);
                 treeInfo.MaxValue = Math.Max(node.val, rightTreeInfo.MaxValue);
-                treeInfo.LargestBstNodes = 1 + leftTreeInfo.LargestBstNodes + rightTreeInfo.LargestBstNodes;
+                treeInfo.LargestBst = 1 + leftTreeInfo.LargestBst + rightTreeInfo.LargestBst;
             }
             //Till this node, we dont form a BST, so we return extreme min max. Any parent form tree contains this branch will not be valid BST
             else
             {
                 treeInfo.MinValue = int.MinValue;
                 treeInfo.MaxValue = int.MaxValue;
-                treeInfo.LargestBstNodes = Math.Max(leftTreeInfo.LargestBstNodes, rightTreeInfo.LargestBstNodes);
+                treeInfo.LargestBst = Math.Max(leftTreeInfo.LargestBst, rightTreeInfo.LargestBst);
             }
 
             return treeInfo;
