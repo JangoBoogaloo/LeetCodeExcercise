13c2cbecc9d98a0922ac4bb771539e4d749d0e22
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-03-06 20:39:00 -0500

    solution

--- leetcode/LinkedListTests/LinkedList_114.cs
+++ leetcode/LinkedListTests/LinkedList_114.cs
@@ -1,7 +1,31 @@
-﻿namespace LinkedListTests;
+﻿using BinaryTree;
+
+namespace LinkedListTests;
 
 [TestFixture]
 internal class LinkedList_114
 {
-    
+    class Solution {
+        public void Flatten(TreeNode root) {
+            if (root is null) return;
+
+            while (root is not null)
+            {
+                if (root.left is not null)
+                {
+                    var rightMost = root.left;
+                    while (rightMost.right is not null)
+                    {
+                        rightMost = rightMost.right;
+                    }
+
+                    rightMost.right = root.right;
+                    root.right = root.left;
+                    root.left = null;
+                }
+
+                root = root.right;
+            }
+        }
+    }
 }
