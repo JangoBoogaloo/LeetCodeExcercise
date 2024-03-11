0a3c58b550cb47d59239a509a57bf736efb5284c
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-03-11 09:56:54 -0400

    328. Odd Even Linked List

---
+++ leetcode/LinkedListTests/LinkedList_328.cs
@@ -0,0 +1,23 @@
+﻿namespace LinkedListTests;
+
+[TestFixture]
+internal class LinkedList_328
+{
+    class Solution {
+        public ListNode OddEvenList(ListNode head) {
+            if(head is null) return head;
+            var odd = head;
+            var even = head.next;
+            var evenHead = even;
+        
+            while(even is not null && even.next is not null) {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
+    }
+}
