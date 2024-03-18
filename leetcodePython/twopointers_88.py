f5e0e2b0800c581313f80c63e99e30637cef13c4
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-03-17 21:30:25 -0400

    88. Merge Sorted Array

---
+++ leetcodePython/twopointers_88.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    solution = Solution()
+    solution.merge([1,2,3,4,None,None,None], 4, [5,6,7], 3)
