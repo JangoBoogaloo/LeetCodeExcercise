a304f3be78a649ee5e6986702a9aff6b707a3d6f
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-03-26 09:29:46 -0400

    Review sliding window

--- leetcodePython/SlidingWindow/slidingwindow_3.py
+++ leetcodePython/SlidingWindow/slidingwindow_3.py
@@ -1,29 +1,44 @@
+class SolutionBruteforce:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        max_length = 0
+        for start in range(0, n):
+            chars = set()
+            for end in range(start, n):
+                if s[end] in chars:
+                    break
+                max_length = max(max_length, end - start + 1)
+                chars.add(s[end])
+        return max_length
+
+
 class SolutionSlidingWindow:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right = 0, 0
-        char_set = set()
+        chars = set()
         max_length = 0
-        for right in range(len(s)):
+        while right < len(s):
             new_char = s[right]
-            while new_char in char_set:
+            while new_char in chars:
                 old_char = s[left]
-                char_set.remove(old_char)
+                chars.remove(old_char)
                 left += 1
-            char_set.add(new_char)
-            max_length = max(max_length, right - left + 1)
+            chars.add(new_char)
+            max_length = max(max_length, right-left+1)
+            right += 1
         return max_length
 
 
 class SolutionSlidingWindowRepeatIndexJump:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right = 0, 0
-        existing_char_map = {}
+        char_index = {}
         max_length = 0
-        for right in range(len(s)):
+        while right < len(s):
             new_char = s[right]
-            if new_char in existing_char_map:
-                prev_char_index = existing_char_map[new_char]
-                left = max(prev_char_index+1, left)
-            existing_char_map[new_char] = right
-            max_length = max(max_length, right - left + 1)
+            if new_char in char_index:
+                left = max(char_index[new_char]+1, left)
+            max_length = max(max_length, right-left+1)
+            char_index[new_char] = right
+            right += 1
         return max_length
