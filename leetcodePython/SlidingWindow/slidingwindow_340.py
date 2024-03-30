09633cca375c541dd7e80f3667c33a81019c78e5
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-03-30 10:41:50 -0400

    clean up

--- leetcodePython/SlidingWindow/slidingwindow_340.py
+++ leetcodePython/SlidingWindow/slidingwindow_340.py
@@ -25,12 +25,12 @@ class SolutionMaxWindowSlidingWindow:
             counter[s[right]] += 1
 
             # we don't care the current window valid, we only need to find a max window
-            if len(counter) <= k:
-                max_size += 1
-            else:
+            if len(counter) > k:
                 # when distinct is more than k, we shift max window but not increase window
                 counter[s[right - max_size]] -= 1
                 if counter[s[right - max_size]] == 0:
                     del counter[s[right - max_size]]
+            else:
+                max_size += 1
 
         return max_size
