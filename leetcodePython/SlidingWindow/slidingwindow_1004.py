3092c50c419576aedf63b1f0ae916b8fc736953c
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-04-09 14:26:44 -0400

    1004. Max Consecutive Ones III

--- leetcodePython/SlidingWindow/slidingwindow_1004.py
+++ leetcodePython/SlidingWindow/slidingwindow_1004.py
@@ -3,14 +3,13 @@ from typing import List
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        zero_count = 0
-        left = 0
-
+        freq = left = right = 0
         for right in range(len(nums)):
-            if not nums[right]:
-                zero_count += 1
-            if zero_count > k:
-                if not nums[left]:
-                    zero_count -= 1
+            if nums[right] == 1:
+                freq += 1
+            replace = right - left + 1 - freq
+            if replace > k:
+                if nums[left] == 1:
+                    freq -= 1
                 left += 1
-        return len(nums) - left
+        return right - left + 1
