aed3042141a34ca5f833ad41f5340bf34e5c8b68
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-05-19 21:39:14 -0400

    document

--- leetcodePython/Stack/stack_772.py
+++ leetcodePython/Stack/stack_772.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def evaluate(x, y, operator):
+    def _evaluate(x, y, operator):
         if operator == "+":
             return x
         if operator == "-":
@@ -26,12 +26,13 @@ class Solution:
                 stack.append(prev_op)
                 prev_op = '+'
             else:  # it's '+-*/' or ')' or '!'
-                if prev_op in '*/':
-                    stack.append(self.evaluate(stack.pop(), curr_num, prev_op))
-                else:
-                    stack.append(self.evaluate(curr_num, 0, prev_op))
+                if prev_op in '*/':  # mult and div will all be calculated
+                    stack.append(self._evaluate(stack.pop(), curr_num, prev_op))
+                else:  # plus and sub will all be stacked with sign
+                    stack.append(self._evaluate(curr_num, 0, prev_op))
+                # reset, previous number is already stacked
                 curr_num = 0
-                if ch == ')':
+                if ch == ')':  # close, sum up all the number in bracket: in stack until we hit an op
                     while type(stack[-1]) == int:
                         curr_num += stack.pop()
                     prev_op = stack.pop()
