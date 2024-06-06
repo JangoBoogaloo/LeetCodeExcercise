5b3a9f97c4ee4a769ad55c1e8434017b2b087c73
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-06-06 09:06:02 -0400

    easier to understand

--- leetcodePython/Stack/stack_84.py
+++ leetcodePython/Stack/stack_84.py
@@ -6,7 +6,8 @@ class Solution:
         increase_stack = [-1]
         max_area = 0
         for i in range(len(heights)):
-            while increase_stack[-1] != -1 and heights[increase_stack[-1]] >= heights[i]:
+            new_height = heights[i]
+            while increase_stack[-1] != -1 and heights[increase_stack[-1]] >= new_height:
                 current_height = heights[increase_stack.pop()]
                 prev_height_index = increase_stack[-1]
                 current_width = i - prev_height_index - 1
