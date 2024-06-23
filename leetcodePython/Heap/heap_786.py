7b192591f52dfd186fc5a268bc6a0ad222b9c264
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-06-22 21:05:58 -0400

    format data

--- leetcodePython/Heap/heap_786.py
+++ leetcodePython/Heap/heap_786.py
@@ -1,19 +1,20 @@
 from typing import List
 from heapq import *
 
+
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         min_heap = []
-        for i in range(len(arr)-1):
-            heappush(min_heap, (arr[i]/arr[-1], i, len(arr)-1))
-        for _ in range(k-1):
+        for i in range(len(arr) - 1):
+            heappush(min_heap, (arr[i] / arr[-1], i, len(arr) - 1))
+        for _ in range(k - 1):
             min_data = heappop(min_heap)
             numerator_i = min_data[1]
             denominator_i = min_data[2] - 1
 
             if denominator_i > numerator_i:
                 heappush(min_heap, (
-                    arr[numerator_i]/arr[denominator_i],
+                    arr[numerator_i] / arr[denominator_i],
                     numerator_i,
                     denominator_i
                 ))
