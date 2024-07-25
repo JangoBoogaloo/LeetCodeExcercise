8bc12d64956306352ff5e5c39d3b628821175d2f
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-07-25 08:28:52 -0400

    Using Cyclic Replacements

--- leetcodePython/TwoPointers/twopointers_189.py
+++ leetcodePython/TwoPointers/twopointers_189.py
@@ -20,3 +20,21 @@ class SolutionReverse:
 
         # [2, 0, 1]
         partial_reverse(nums, k, len(nums)-1)
+
+
+class SolutionCyclicReplacement:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+        start_i = count = 0
+        while count < n:
+            current_i = start_i
+            prev_num = nums[start_i]
+            while True:
+                next_i = (current_i + k) % n
+                nums[next_i], prev_num = prev_num, nums[next_i]
+                current_i = next_i
+                count += 1
+                if start_i == current_i:
+                    break
+            start_i += 1
