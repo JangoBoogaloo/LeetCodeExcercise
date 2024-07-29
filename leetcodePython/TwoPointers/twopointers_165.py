de84372d4e04ac268a917e3f04e59c9e5bf6c260
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-07-29 08:02:39 -0400

    twopointers approach

--- leetcodePython/TwoPointers/twopointers_165.py
+++ leetcodePython/TwoPointers/twopointers_165.py
@@ -9,3 +9,34 @@ class SolutionSplit:
             if num1 != num2:
                 return 1 if num1 > num2 else -1
         return 0
+
+
+class SolutionTwoPointers:
+    def _get_next_chunk(self, version: str, length: int, start: int) -> tuple[int, int]:
+        if start > length - 1:
+            return 0, start
+        end = start
+        while end < length and version[end] != '.':
+            end += 1
+        num = -1
+        if end != length - 1:
+            num = int(version[start:end])
+        else:
+            num = int(version[start:length])
+        start = end + 1
+        return num, start
+
+    def compareVersion(self, version1: str, version2: str) -> int:
+        len1, len2 = len(version1), len(version2)
+        i1, i2 = 0, 0
+        while i1 < len1 or i2 < len2:
+            num1, i1 = self._get_next_chunk(version1, len1, i1)
+            num2, i2 = self._get_next_chunk(version2, len2, i2)
+            if num1 != num2:
+                return 1 if num1 > num2 else -1
+        return 0
+
+
+if __name__ == '__main__':
+    sol = SolutionTwoPointers()
+    sol.compareVersion('1.0.1', '1')
