0f95aa50660baeb7f8402145f9664e49297b3515
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-08-03 15:50:03 -0400

    comment

--- leetcodePython/PrefixSum/prefixsum_1094.py
+++ leetcodePython/PrefixSum/prefixsum_1094.py
@@ -4,6 +4,8 @@ from typing import List
 
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        # we could have 1000 position from left to right (west to east), start from index 1
+        # this variable record number of passenger change at each position, by default there's no change
         passenger_change = [0] * 1001
         for passenger, start, end in trips:
             passenger_change[start] += passenger
