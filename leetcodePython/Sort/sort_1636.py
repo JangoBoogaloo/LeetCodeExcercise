afea5b7f92c00c2fe1bc02a3ab06b5f68db861dc
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-08-08 08:41:33 -0400

    clean solution

--- leetcodePython/Sort/sort_1636.py
+++ leetcodePython/Sort/sort_1636.py
@@ -6,11 +6,4 @@ from typing import List
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
         frequency = collections.Counter(nums)
-
-        def compare(a: int, b: int) -> int:
-            if frequency[a] == frequency[b]:
-                return b - a
-            return frequency[a] - frequency[b]
-
-        nums.sort(key=cmp_to_key(compare))
-        return nums
+        return sorted(nums, key=lambda x: (frequency[x], -x))
