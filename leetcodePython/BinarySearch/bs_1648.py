deea205629127ec3b959bdff39c4d21a89f1580d
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-08-15 22:11:49 -0400

    use binary search instead

--- leetcodePython/BinarySearch/bs_1648.py
+++ leetcodePython/BinarySearch/bs_1648.py
@@ -3,23 +3,34 @@ from typing import List
 
 
 class Solution:
-    def arithmeticSum(self, a_n: int, a_1: int, n: int) -> int:
-        return (a_n + a_1 + 1) * n // 2
+    # if we set a base_price, how many balls can we sell with more than this price
+    # get maximum price (and maximum balls sold) will be our greedy strategy
+    @staticmethod
+    def _sold_balls(inventory: List[int], base_price: int) -> int:
+        balls = 0
+        for price in inventory:
+            if price > base_price:
+                balls += (price - base_price)
+        return balls
 
     def maxProfit(self, inventory: List[int], orders: int) -> int:
-        inventory.sort(reverse=True) # inventory high to low
-        inventory += [0]
-        ans = 0
-        greater_element_count = 1
-        for i in range(len(inventory)-1):
-            if inventory[i] > inventory[i+1]:
-                n = inventory[i] - inventory[i+1]
-                if greater_element_count*n < orders:
-                    ans += greater_element_count * self.arithmeticSum(inventory[i], inventory[i+1], n)
-                    orders -= greater_element_count*n
-                else:
-                    q, r = divmod(orders, greater_element_count)
-                    magic = greater_element_count*(2*inventory[i] - q + 1) * q//2 + r*(inventory[i] - q)
-                    ans += magic
-                    return ans % 1_000_000_007
-            greater_element_count += 1
+        left, right = 0, max(inventory)
+        base_price = right
+        while left < right:
+            base_price = (left + right) // 2
+            if self._sold_balls(inventory, base_price) > orders:
+                left = base_price + 1
+            else:
+                right = base_price
+
+        sold_balls = 0
+        total = 0
+        for price in inventory:
+            if price > right:
+                sold_balls += price - right
+                bigger_price_sum = (price + right + 1) * (price - right) // 2
+                total += bigger_price_sum
+
+        if sold_balls < orders:
+            total += (orders - sold_balls) * right
+        return total % 1_000_000_007
