fd479c0e4c62847843f1a57b9ea5c8d671671411
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-08-30 08:56:06 -0400

    better answer, 1 iteration

--- leetcodePython/Sort/sort_1200.py
+++ leetcodePython/Sort/sort_1200.py
@@ -4,12 +4,15 @@ from typing import List
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr.sort()
-        diff = []
+        minDiff = float('inf')
+        result = []
+
         for i in range(len(arr) - 1):
-            diff.append(abs(arr[i + 1] - arr[i]))
-        min_diff = min(diff)
-        ans = []
-        for i in range(len(arr) - 1):
-            if (arr[i + 1] - arr[i]) == min_diff:
-                ans.append([arr[i], arr[i + 1]])
-        return ans
+            diff = arr[i + 1] - arr[i]
+
+            if diff < minDiff:
+                minDiff = diff
+                result = [[arr[i], arr[i + 1]]]
+            elif diff == minDiff:
+                result.append([arr[i], arr[i + 1]])
+        return result
