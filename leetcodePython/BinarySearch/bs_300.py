d781058859912a5b0e9e6029622bbd9ebddf2770
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-09-01 12:52:32 -0400

    more description

--- leetcodePython/BinarySearch/bs_300.py
+++ leetcodePython/BinarySearch/bs_300.py
@@ -3,11 +3,13 @@ from typing import List
 
 class SolutionDP:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # the length of the longest increasing subsequence that ends with the i th element
         dp_sequence_length = [1] * len(nums)
 
         # kind of slide window or two pointer
         for right in range(1, len(nums)):
             for left in range(right):
                 if nums[right] > nums[left]:
-                    dp_sequence_length[right] = max(dp_sequence_length[right], dp_sequence_length[left] + 1)
+                    left_sequence_with_right = dp_sequence_length[left] + 1
+                    dp_sequence_length[right] = max(dp_sequence_length[right], left_sequence_with_right)
         return max(dp_sequence_length)
