e1760f1357dae7ce8b8f03a66bae7dcae022a498
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-09-04 08:41:27 -0400

    Binary search solution

--- leetcodePython/BinarySearch/bs_2251.py
+++ leetcodePython/BinarySearch/bs_2251.py
@@ -1,3 +1,4 @@
+import bisect
 import collections
 import heapq
 from typing import List
@@ -52,3 +53,19 @@ class SolutionHeap:
         return [ans[time] for time in people]
 
 
+class SolutionSeparateStartEndArrayBS:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        starts = []
+        ends = []
+
+        for start, end in flowers:
+            starts.append(start)
+            ends.append(end + 1)
+        starts.sort()
+        ends.sort()
+        ans = []
+        for arrive_time in people:
+            idx_valid_start = bisect.bisect_right(starts, arrive_time)
+            idx_valid_end = bisect.bisect_right(ends, arrive_time)
+            ans.append(idx_valid_start - idx_valid_end)
+        return ans
