dc4cb14423f463b0426d181369b3e98decfea5bb
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-09-16 14:58:51 -0400

    Add Union Find solution

--- leetcodePython/matrix/matrix_200.py
+++ leetcodePython/matrix/matrix_200.py
@@ -94,3 +94,73 @@ class SolutionDFSNoModify:
                     islands += 1
                 visited[row][col] = True
         return islands
+
+
+class UnionFind:
+    def __init__(self, grid: List[List[str]]):
+        self._count = 0
+        self._parent = []
+        self._rank = []
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1":
+                    cell_index = row * len(grid[0]) + col
+                    self._parent.append(cell_index)
+                    self._count += 1
+                else:
+                    self._parent.append(-1)
+                self._rank.append(0)
+
+    def find(self, data: int) -> int:
+        if self._parent[data] != data:
+            self._parent[data] = self.find(self._parent[data])
+        return self._parent[data]
+
+    def union(self, a: int, b: int) -> None:
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a != root_b:
+            if self._rank[root_a] > self._rank[root_b]:
+                self._parent[root_b] = root_a
+            elif self._rank[root_a] < self._rank[root_b]:
+                self._parent[root_a] = root_b
+            else:
+                self._parent[root_b] = root_a
+                self._rank[root_a] += 1
+            self._count -= 1
+
+    def count(self) -> int:
+        return self._count
+
+
+class SolutionUnionFind:
+    @staticmethod
+    def _join_new_land(grid: List[List[str]], uf: UnionFind, land: tuple[int, int], new_land: tuple[int, int]) -> None:
+        r_new, c_new = new_land
+        if r_new < 0 or c_new < 0:
+            return
+        if r_new > len(grid) - 1 or c_new > len(grid[0]) - 1:
+            return
+        if grid[r_new][c_new] == "0":
+            return
+        r, c = land
+        land_id = r * len(grid[0]) + c
+        new_land_id = r_new * len(grid[0]) + c_new
+        uf.union(land_id, new_land_id)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        rows = len(grid)
+        cols = len(grid[0])
+        uf = UnionFind(grid)
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == "0":
+                    continue
+                self._join_new_land(grid, uf, (row, col), (row - 1, col))
+                self._join_new_land(grid, uf, (row, col), (row + 1, col))
+                self._join_new_land(grid, uf, (row, col), (row, col - 1))
+                self._join_new_land(grid, uf, (row, col), (row, col + 1))
+        return uf.count()
