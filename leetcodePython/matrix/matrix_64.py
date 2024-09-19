160e84ff93c523a608a145bdd7cdfabea95bdb68
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-09-19 13:17:45 -0400

    64. Minimum Path Sum

--- leetcodePython/matrix/matrix_64.py
+++ leetcodePython/matrix/matrix_64.py
@@ -1,6 +1,13 @@
 from typing import List
 
 
-class Solution:
+class SolutionRecursion:
+    def _calculate_sum(self, grid: List[List[int]], row: int, col: int) -> float:
+        if row == len(grid) or col == len(grid[0]):
+            return float("inf")
+        if row == len(grid) - 1 and col == len(grid[0]) - 1:
+            return grid[row][col]
+        return grid[row][col] + min(self._calculate_sum(grid, row + 1, col), self._calculate_sum(grid, row, col + 1))
+
     def minPathSum(self, grid: List[List[int]]) -> int:
-        return 0
+        return int(self._calculate_sum(grid, 0, 0))
