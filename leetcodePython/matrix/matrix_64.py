92a8884bf4f515eb800ad4a7528c6eeb556be6d4
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-09-19 13:30:48 -0400

    in-place dp solution

--- leetcodePython/matrix/matrix_64.py
+++ leetcodePython/matrix/matrix_64.py
@@ -11,3 +11,18 @@ class SolutionRecursion:
 
     def minPathSum(self, grid: List[List[int]]) -> int:
         return int(self._calculate_sum(grid, 0, 0))
+
+
+class SolutionInPlaceDP:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        for row in range(1, rows):
+            grid[row][0] += grid[row - 1][0]
+
+        for col in range(1, cols):
+            grid[0][col] += grid[0][col - 1]
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                grid[row][col] += min(grid[row - 1][col], grid[row][col - 1])
+        return grid[-1][-1]
