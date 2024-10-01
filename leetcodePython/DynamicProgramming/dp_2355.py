2c9527032651bf7b4e47e4b303ea1acf5b72e8f4
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-09-30 22:06:31 -0400

    comment to explain

--- leetcodePython/DynamicProgramming/dp_2355.py
+++ leetcodePython/DynamicProgramming/dp_2355.py
@@ -22,14 +22,21 @@ class Solution:
             while stack:
                 left = stack[-1]
                 # books[j] < books[i]−(i−j)
-                # naturally form the increasing sequence from left to right
+                # if we can't form a strictly 1 diff increasing sequence between left and right, now this is smaller, we keep stack
                 if books[left] < books[right] - (right - left):
                     break
                 stack.pop()
             if not stack:
                 dp[right] = self._calculateSum(books, 0, right)
             else:
+                # guarantee data at index left is smaller than the 1 diff sequence
                 left = stack[-1]
+                # now the sum becomes whatever sum the left can form and the sum formed by left + 1 to right
                 dp[right] = dp[left] + self._calculateSum(books, left + 1, right)
             stack.append(right)
         return max(dp)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    ans = sol.maximumBooks([2, 7, 5, 4])
