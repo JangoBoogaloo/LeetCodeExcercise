59b21768dc0a206c6944dbbaec6a6b7e838faf74
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-10-03 21:22:54 -0400

    Memory optimization

--- leetcodePython/DynamicProgramming/dp_1567.py
+++ leetcodePython/DynamicProgramming/dp_1567.py
@@ -28,6 +28,32 @@ class SolutionDP:
         return ans
 
 
-if __name__ == "__main__":
-    sol = SolutionDP()
-    sol.getMaxLen([0,1,-2,-3,-4])
+class SolutionOptimize:
+    def getMaxLen(self, nums: List[int]) -> int:
+        size = len(nums)
+        positive_array_length = 0
+        negative_array_length = 0
+        if nums[0] > 0:
+            positive_array_length = 1
+        elif nums[0] < 0:
+            negative_array_length = 1
+
+        ans = positive_array_length
+
+        for i in range(1, size):
+            if nums[i] > 0:
+                positive_array_length += 1
+                if negative_array_length > 0:
+                    negative_array_length += 1
+            elif nums[i] < 0:
+                if negative_array_length > 0:
+                    positive_array_length, negative_array_length = negative_array_length, positive_array_length
+                    positive_array_length += 1
+                    negative_array_length += 1
+                else:
+                    negative_array_length = 1 + positive_array_length
+                    positive_array_length = 0
+            else:  # nums[i] == 0
+                positive_array_length, negative_array_length = 0, 0
+            ans = max(ans, positive_array_length)
+        return ans
