1471fd656c661a3e32d066d67656271bbe4a59ed
Jango <1152134+JangoBoogaloo@users.noreply.github.com>
2024-10-07 13:10:59 -0400

    Construct Binary Tree from Order Traversals (105, 106, 889) (#1)
    * construct binary tree base on traversal
    
    * code cleanup
    
    * clean up
    
    * Solution for 889
    
    * memory & time efficient solution
    
    * time and memory efficient
    
    * iterative
    
    * Python solution 105
    
    * Python 106 solution
    
    * 889 python solution

---
+++ LeetCode/BinaryTreeTests/ConstructBinaryTree_106.cs
@@ -0,0 +1,50 @@
+﻿using BinaryTree;
+
+namespace BinaryTreeTests;
+
+[TestFixture]
+class ConstructBinaryTree_106
+{
+    private static IReadOnlyList<ConstructBinaryTreeTestCase> _constructBinaryTree106TestCase = new[]
+    {
+        new ConstructBinaryTreeTestCase()
+        {
+            InOrder = new[] { 9, 3, 15, 20, 7 },
+            PostOrder = new []{9,15,7,20,3},
+            Expected = new int?[] { 3, 9, 20, null, null, 15, 7 },
+        }
+    };
+    
+    [TestCaseSource(nameof(_constructBinaryTree106TestCase))]
+    public void TestConstructBinaryTree_106(ConstructBinaryTreeTestCase testcase)
+    {
+        var solution = new Solution();
+        var tree = solution.BuildTree(testcase.InOrder!, testcase.PostOrder!);
+        var actual = TreeNode.ToList(tree).ToArray();
+        CollectionAssert.AreEqual(testcase.Expected, actual);
+    }
+
+    private class Solution
+    {
+        public TreeNode BuildTree(int[] inorder, int[] postorder)
+        {
+            var inorderIndexMap = new Dictionary<int,int>();
+            for(int i=0; i<inorder.Length; i++) {
+                inorderIndexMap[inorder[i]] = i;
+            }
+            return BuildTreeHelper(postorder.Length-1, 0, inorder.Length-1);
+
+            TreeNode BuildTreeHelper(int postEnd, int inStart, int inEnd)
+            {
+                if(postEnd <0 || inStart > inEnd) return null;
+                var rootVal = postorder[postEnd];
+                var rootIndex = inorderIndexMap[rootVal];
+                var rightTreeLength = inEnd-rootIndex;
+                var root = new TreeNode(rootVal);
+                root.left = BuildTreeHelper(postEnd-1-rightTreeLength, inStart, rootIndex-1);
+                root.right = BuildTreeHelper(postEnd-1, rootIndex+1, inEnd);
+                return root;
+            }
+        }
+    }
+}
