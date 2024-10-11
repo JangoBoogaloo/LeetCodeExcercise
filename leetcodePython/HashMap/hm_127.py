87260fc9cafbc45a3c93bcbb94964ff2cfeb2239
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-10-11 19:52:48 -0400

    better naming

--- leetcodePython/HashMap/hm_127.py
+++ leetcodePython/HashMap/hm_127.py
@@ -8,24 +8,24 @@ class Solution:
             return 0
         size = len(beginWord)
         GENERIC_CHAR = "*"
-        words_combo = collections.defaultdict(list)
+        combination_word_map = collections.defaultdict(list)
         for word in wordList:
             for i in range(size):
                 # Replace a character to generic * so we can easily compare
-                a_combo = word[:i] + GENERIC_CHAR + word[i + 1:]
-                words_combo[a_combo].append(word)
+                a_combination = word[:i] + GENERIC_CHAR + word[i + 1:]
+                combination_word_map[a_combination].append(word)
 
         queue = collections.deque([(beginWord, 1)])
         visited = {beginWord: True}
         while queue:
             curr_word, level = queue.popleft()
             for i in range(size):
-                word_combo = curr_word[:i] + GENERIC_CHAR + curr_word[i + 1:]
-                for word in words_combo[word_combo]:
+                word_combination = curr_word[:i] + GENERIC_CHAR + curr_word[i + 1:]
+                for word in combination_word_map[word_combination]:
                     if word == endWord:
                         return level + 1
                     if word not in visited:
                         visited[word] = True
                         queue.append((word, level + 1))
-                words_combo[word_combo] = []
+                combination_word_map[word_combination] = []
         return 0
