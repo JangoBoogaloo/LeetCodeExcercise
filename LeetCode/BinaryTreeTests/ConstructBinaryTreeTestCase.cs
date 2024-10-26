ae3b9ecbb8900b6e79fd0dd77ceed09bb793fa6e
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-10-26 09:09:24 -0400

    delete C#

--- LeetCode/BinaryTreeTests/ConstructBinaryTreeTestCase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BinaryTreeTests;
-
-public class ConstructBinaryTreeTestCase
-{
-    public int[]? Preorder;
-    public int[]? InOrder;
-    public int[]? PostOrder;
-    public int?[] Expected;
-}
