2be20d5c6ef308b9ce3d00d357fe9689884f6cf2
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-10-28 14:03:24 -0400

    remove brute force

--- leetcodePython/SlidingWindow/slidingwindow_3.py
+++ leetcodePython/SlidingWindow/slidingwindow_3.py
@@ -1,17 +1,3 @@
-class SolutionBruteforce:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        max_length = 0
-        for start in range(0, n):
-            chars = set()
-            for end in range(start, n):
-                if s[end] in chars:
-                    break
-                max_length = max(max_length, end - start + 1)
-                chars.add(s[end])
-        return max_length
-
-
 class SolutionSlidingWindow:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right = 0, 0
