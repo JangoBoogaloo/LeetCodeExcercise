90df39ec655bdbf91a8b932bf563143e520bfbd5
Jango <xiangguoxg@gmail.com>
2024-10-31 18:26:18 -0400

    cleanup

--- leetcodePython/SlidingWindow/slidingwindow_159.py
+++ leetcodePython/SlidingWindow/slidingwindow_159.py
@@ -3,19 +3,18 @@ from collections import Counter
 
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: 'str') -> 'int':
-        max_size = 0
-        counter = Counter()
+        # sol = slidingwindow_340.SolutionMaxWindowSlidingWindow()
+        # return sol.lengthOfLongestSubstringKDistinct(s, 2)
 
+        window_freq = Counter()
+        left = 0
         for right in range(len(s)):
-            counter[s[right]] += 1
-
-            # we don't care the current window valid, we only need to find a max window
-            if len(counter) > 2:
-                # when distinct is more than k, we shift max window but not increase window
-                counter[s[right - max_size]] -= 1
-                if counter[s[right - max_size]] == 0:
-                    del counter[s[right - max_size]]
-            else:
-                max_size += 1
-
-        return max_size
+            new_ch = s[right]
+            window_freq[new_ch] += 1
+            if len(window_freq) > 2:
+                old_ch = s[left]
+                window_freq[old_ch] -= 1
+                if window_freq[old_ch] == 0:
+                    window_freq.pop(old_ch)
+                left += 1
+        return right - left + 1
