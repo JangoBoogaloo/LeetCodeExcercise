c70f55e680e8ee7176b16ea7ca26c351592cd861
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-11-03 05:53:06 -0500

    clean up

--- leetcodePython/SlidingWindow/slidingwindow_992.py
+++ leetcodePython/SlidingWindow/slidingwindow_992.py
@@ -2,23 +2,24 @@ import collections
 from typing import List
 
 class SolutionSubSetApproach:
-    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        # With at most K - With at most K-1 = With exactly K
-        return self.__subarayWithAtMostKDistinct(nums, k) - self.__subarayWithAtMostKDistinct(nums,k-1)
-
-    def __subarayWithAtMostKDistinct(self, nums: List[int], k: int) -> int:
-        left, right, total_count = 0, 0, 0
-        num_freq = collections.Counter()
+    def _subarrayWithAtMostX(self, nums: List[int], x: int) -> int:
+        left, total, diff = 0, 0, 0
+        freq = collections.Counter()
         for right in range(len(nums)):
-            num_freq[nums[right]] += 1
-            while k < len(num_freq):
-                num_freq[nums[left]] -= 1
-                if num_freq[nums[left]] == 0:
-                    del num_freq[nums[left]]
+            freq[nums[right]] += 1
+            if freq[nums[right]] == 1:
+                diff += 1
+            while diff > x:
+                freq[nums[left]] -= 1
+                if freq[nums[left]] == 0:
+                    diff -= 1
                 left += 1
-            window_array_count = right - left + 1
-            total_count += window_array_count
-        return total_count
+            total += right - left + 1
+        return total
+
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self._subarrayWithAtMostX(nums, k) - self._subarrayWithAtMostX(nums, k-1)
+
 
 class SolutionSlidingWindow:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
