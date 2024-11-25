ecf649c409bc242fcc0eeddea3a70826d3a70481
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-11-25 10:09:44 -0500

    cleanup

--- leetcodePython/matrix/matrix_64.py
+++ leetcodePython/matrix/matrix_64.py
@@ -1,19 +1,7 @@
 from typing import List
 
 
-class SolutionRecursion:
-    def _calculate_sum(self, grid: List[List[int]], row: int, col: int) -> float:
-        if row == len(grid) or col == len(grid[0]):
-            return float("inf")
-        if row == len(grid) - 1 and col == len(grid[0]) - 1:
-            return grid[row][col]
-        return grid[row][col] + min(self._calculate_sum(grid, row + 1, col), self._calculate_sum(grid, row, col + 1))
-
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        return int(self._calculate_sum(grid, 0, 0))
-
-
-class SolutionInPlaceDP:
+class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         for row in range(1, rows):
@@ -26,3 +14,15 @@ class SolutionInPlaceDP:
             for col in range(1, cols):
                 grid[row][col] += min(grid[row - 1][col], grid[row][col - 1])
         return grid[-1][-1]
+
+
+class SolutionRecursion:
+    def _calculate_sum(self, grid: List[List[int]], row: int, col: int) -> float:
+        if row == len(grid) or col == len(grid[0]):
+            return float("inf")
+        if row == len(grid) - 1 and col == len(grid[0]) - 1:
+            return grid[row][col]
+        return grid[row][col] + min(self._calculate_sum(grid, row + 1, col), self._calculate_sum(grid, row, col + 1))
+
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        return int(self._calculate_sum(grid, 0, 0))
