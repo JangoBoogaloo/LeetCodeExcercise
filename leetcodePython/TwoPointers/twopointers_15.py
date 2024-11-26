b51dd27fc6649afc8b0ecdbe29176a7a1eb52ada
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-11-26 07:51:17 -0500

    cleanup

--- leetcodePython/TwoPointers/twopointers_15.py
+++ leetcodePython/TwoPointers/twopointers_15.py
@@ -2,35 +2,28 @@ from typing import List
 
 
 class Solution:
+    def _threeSumStartFromIndex(self, nums: List[int], index: int, target: int, result: List[List[int]]):
+        left = index + 1
+        right = len(nums) - 1
+        while left < right:
+            currSum = nums[index] + nums[left] + nums[right]
+            if currSum > target:
+                right -= 1
+            elif currSum < target:
+                left += 1
+            else:
+                result.append([nums[index], nums[left], nums[right]])
+                left += 1
+                right -= 1
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result = []
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            # this check is to avoid duplicate
             if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSum(nums, i, result)
+                self._threeSumStartFromIndex(nums, i, 0, result)
         return result
-
-    def twoSum(self, nums: List[int], i: int, result: List[List[int]]):
-        left = i + 1
-        right = len(nums) - 1
-        while left < right:
-            threesum = nums[i] + nums[left] + nums[right]
-            if threesum < 0:
-                left += 1
-            elif threesum > 0:
-                right -= 1
-            else:
-                result.append([nums[i], nums[left], nums[right]])
-                left += 1
-                right -= 1
-                while left < right and nums[left] == nums[left-1]:
-                    left += 1
-
-
-if __name__ == '__main__':
-    solution = Solution()
-    result = solution.threeSum([-1,0,1,2,-1,-4])
-    print(result)
