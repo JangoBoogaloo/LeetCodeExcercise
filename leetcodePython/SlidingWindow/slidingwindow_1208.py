7c58dcac78b5fec3597c0f9a0536350928c3b748
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-12-02 07:58:40 -0500

    revert

--- leetcodePython/SlidingWindow/slidingwindow_1208.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        ans = left = right = 0
-        for right, ch in enumerate(s):
-            maxCost -= abs(ord(ch) - ord(t[right]))
-            if maxCost < 0:
-                maxCost += abs(ord(s[left]) - ord(t[left]))
-                left += 1
-        return right - left + 1
