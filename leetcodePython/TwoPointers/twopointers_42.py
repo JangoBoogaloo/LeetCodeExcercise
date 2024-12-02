ea15c2a5c562756dd32e540100d18554b5dc7b63
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2024-12-01 20:56:50 -0500

    cleanup

--- leetcodePython/TwoPointers/twopointers_42.py
+++ leetcodePython/TwoPointers/twopointers_42.py
@@ -3,20 +3,20 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        leftMax, rightMax = 0, 0
+        leftWall, rightWall = 0, 0
         left, right = 0, len(height) - 1
         rain = 0
         while left < right:
             if height[left] < height[right]:
-                if height[left] < leftMax:
-                    rain += leftMax - height[left]
+                if height[left] < leftWall:
+                    rain += leftWall - height[left]
                 else:
-                    leftMax = height[left]
+                    leftWall = height[left]
                 left += 1
             else:
-                if height[right] < rightMax:
-                    rain += rightMax - height[right]
+                if height[right] < rightWall:
+                    rain += rightWall - height[right]
                 else:
-                    rightMax = height[right]
+                    rightWall = height[right]
                 right -= 1
         return rain
