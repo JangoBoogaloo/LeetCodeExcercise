d3a45fdd37aa6193fd9c93e7cef1abb08b6ef1f4
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2025-01-05 13:57:26 -0500

    cleanup

--- leetcodePython/Stack/stack_1944.py
+++ leetcodePython/Stack/stack_1944.py
@@ -3,12 +3,15 @@ from typing import List
 
 class Solution:
     def canSeePersonsCount(self, heights: List[int]) -> List[int]:
-        ans = [0] * len(heights)
-        stack = []
+        answer = [0] * len(heights)
+        decreaseHeightIndex = []
         for i in range(len(heights)):
-            while stack and heights[i] >= heights[stack[-1]]:
-                ans[stack.pop()] += 1
-            if stack:
-                ans[stack[-1]] += 1
-            stack.append(i)
-        return ans
+            while decreaseHeightIndex and heights[i] >= heights[decreaseHeightIndex[-1]]:
+                answer[decreaseHeightIndex[-1]] += 1
+                decreaseHeightIndex.pop()
+
+            if decreaseHeightIndex:
+                answer[decreaseHeightIndex[-1]] += 1
+
+            decreaseHeightIndex.append(i)
+        return answer
