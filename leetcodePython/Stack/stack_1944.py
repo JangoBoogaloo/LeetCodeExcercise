bab3db648375c4307836545d5291515454988500
xiangguo-getluckyvr <xiangguoxg@gmail.com>
2025-01-08 08:35:54 -0500

    cleanup

--- leetcodePython/Stack/stack_1944.py
+++ leetcodePython/Stack/stack_1944.py
@@ -9,9 +9,32 @@ class Solution:
             while decreaseHeightIndex and heights[i] >= heights[decreaseHeightIndex[-1]]:
                 answer[decreaseHeightIndex[-1]] += 1
                 decreaseHeightIndex.pop()
-
+            # latest height remain in stack is higher than current height
             if decreaseHeightIndex:
                 answer[decreaseHeightIndex[-1]] += 1
 
             decreaseHeightIndex.append(i)
         return answer
+
+
+class VisibleIndex:
+    def canSeePersons(self, heights: List[int]) -> List[List[int]]:
+        answer: List[List[int]]
+        answer = [[] for _ in range(len(heights))]
+        decreaseHeightIndex = []
+        for i in range(len(heights)):
+            while decreaseHeightIndex and heights[i] >= heights[decreaseHeightIndex[-1]]:
+                answer[decreaseHeightIndex[-1]].append(i)
+                decreaseHeightIndex.pop()
+
+            # latest height remain in stack is higher than current height
+            if decreaseHeightIndex:
+                answer[decreaseHeightIndex[-1]].append(i)
+
+            decreaseHeightIndex.append(i)
+        return answer
+
+
+if __name__ == '__main__':
+    solution = VisibleIndex()
+    print(solution.canSeePersons([4,5,6]))
