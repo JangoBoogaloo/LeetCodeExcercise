dd67cb98b81009d56de0c66c89b465a9041ef2c4
Jango <xiangguoxg@gmail.com>
2025-02-05 08:51:05 -0500

    DP solution

--- leetcodePython/DynamicProgramming/dp_656.py
+++ leetcodePython/DynamicProgramming/dp_656.py
@@ -3,4 +3,27 @@ from typing import List
 
 class Solution:
     def cheapestJump(self, coins: List[int], maxJump: int) -> List[int]:
+        minCostFrom = [0] * len(coins)
+        nextJumpOf = [-1] * len(coins)
+        for start in range(len(coins)-2, -1, -1):
+            jumpRange = min(start+maxJump+1, len(coins))
+            minCost = float("inf")
+            for end in range(start+1, jumpRange, 1):
+                if coins[end] >= 0:
+                    cost = coins[start] + minCostFrom[end]
+                    if cost < minCost:
+                        minCost = cost
+                        nextJumpOf[start] = end
+            minCostFrom[start] = minCost
+
+        path = []
+        index = 0
+        while index < len(coins) and nextJumpOf[index] > 0:
+            path.append(index + 1)
+            index = nextJumpOf[index]
+
+        if index == len(coins) - 1 and minCostFrom[index] >= 0:
+            path.append(len(coins))
+            return path
+
         return []
