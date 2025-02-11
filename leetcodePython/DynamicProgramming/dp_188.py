f524e8c5dbe41e3d70253ad7912e5fabe1bbb3bb
Jango <xiangguoxg@gmail.com>
2025-02-11 08:22:14 -0500

    cleanup

--- leetcodePython/DynamicProgramming/dp_188.py
+++ leetcodePython/DynamicProgramming/dp_188.py
@@ -18,25 +18,26 @@ class Solution:
 
         # maxBalance_day_transaction_hold[day][used_k][ishold] = balance
         # ishold: 0 nothold, 1 hold
-        maxBalance_day_transaction_hold = [[[-math.inf] * 2 for _ in range(k + 1)] for _ in range(n)]
 
-        # set starting value
-        maxBalance_day_transaction_hold[0][0][0] = 0
-        maxBalance_day_transaction_hold[0][1][1] = -prices[0]
+        maxBalanceHoldAt = [[-math.inf] * k for _ in range(n)]
+        maxBalanceSoldAt = [[-math.inf] * k for _ in range(n)]
+
+        maxBalanceSoldAt[0][0] = 0
+        maxBalanceHoldAt[0][1] = -prices[0]
 
         # fill the array
         for day in range(1, n):
             for usedTransaction in range(k + 1):
-                # transition equation
-                maxBalance_day_transaction_hold[day][usedTransaction][0] = (
-                    max(maxBalance_day_transaction_hold[day - 1][usedTransaction][0],
-                        maxBalance_day_transaction_hold[day - 1][usedTransaction][1] + prices[day]))
-                # you can't hold stock without any transaction
-                if usedTransaction > 0:
-                    maxBalance_day_transaction_hold[day][usedTransaction][1] = max(
-                        maxBalance_day_transaction_hold[day - 1][usedTransaction][1],
-                        maxBalance_day_transaction_hold[day - 1][usedTransaction - 1][0] - prices[day]
-                    )
+                maxBalanceSoldAt[day][usedTransaction] = self._getCurrentMaxFromSold(day, usedTransaction, prices[day], maxBalanceHoldAt, maxBalanceSoldAt)
+                if usedTransaction <= 0:
+                    continue
+                maxBalanceHoldAt[day][usedTransaction] = self._getCurrentMaxFromHold(day, usedTransaction, prices[day], maxBalanceHoldAt, maxBalanceSoldAt)
 
-        res = max(maxBalance_day_transaction_hold[n - 1][j][0] for j in range(k + 1))
-        return res
+        res = max(maxBalanceSoldAt[n - 1][j]for j in range(k + 1))
+        return int(res)
+
+    def _getCurrentMaxFromSold(self, day: int, transaction:int, price,  maxBalanceHoldAt: List[List[float]], maxBalanceSoldAt: List[List[float]]) -> float:
+        return max(maxBalanceSoldAt[day - 1][transaction], maxBalanceHoldAt[day - 1][transaction] + price)
+
+    def _getCurrentMaxFromHold(self, day: int, transaction:int, price,  maxBalanceHoldAt: List[List[float]], maxBalanceSoldAt: List[List[float]]) -> float:
+        return max(maxBalanceSoldAt[day - 1][transaction-1] - price, maxBalanceHoldAt[day - 1][transaction])
