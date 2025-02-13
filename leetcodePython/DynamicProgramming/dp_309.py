217c3c9d3e474a0d937b18b11595080ea80cb01e
Xiang Guo <xiangguoxg@gmail.com>
2025-02-12 20:22:06 -0500

    bottom up

--- leetcodePython/DynamicProgramming/dp_309.py
+++ leetcodePython/DynamicProgramming/dp_309.py
@@ -19,3 +19,21 @@ class Solution:
             maxBalanceHoldAt[day] = max(maxBalanceHoldAt[day-1], maxBalanceResetAt[day-1] - prices[day])
             maxBalanceResetAt[day] = max(maxBalanceResetAt[day-1], maxBalanceSoldAt[day-1])
         return int(max(maxBalanceSoldAt[-1], maxBalanceResetAt[-1]))
+
+
+class SolutionBottomUp:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+
+        maxBalanceSold = float('-inf')
+        maxBalanceHold = -prices[0]
+        maxBalanceReset = 0
+
+        for currentPrice in prices[1:]:
+            prevSold = maxBalanceSold
+            maxBalanceSold = maxBalanceHold + currentPrice
+            maxBalanceHold = max(maxBalanceHold, maxBalanceReset - currentPrice)
+            maxBalanceReset = max(maxBalanceReset, prevSold)
+
+        return max(maxBalanceSold, maxBalanceReset)
