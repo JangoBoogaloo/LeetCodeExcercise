dc390d7b2645079bb0655ed85d01900ca7cddbe5
Xiang Guo <xiangguoxg@gmail.com>
2025-02-12 20:03:49 -0500

    309. Best Time to Buy and Sell Stock with Cooldown

---
+++ leetcodePython/DynamicProgramming/dp_309.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+
+        if len(prices) < 3:
+            return max(prices[1]-prices[0], 0)
+
+        maxBalanceSoldAt: List[float] = [float('-inf')] * len(prices)
+        maxBalanceHoldAt: List[float] = [float('-inf')] * len(prices)
+        maxBalanceResetAt: List[float] = [0] * len(prices)
+
+        for day in range(len(prices)):
+            maxBalanceSoldAt[day] = maxBalanceHoldAt[day-1] + prices[day]
+            maxBalanceHoldAt[day] = max(maxBalanceHoldAt[day-1], maxBalanceResetAt[day-1] - prices[day])
+            maxBalanceResetAt[day] = max(maxBalanceResetAt[day-1], maxBalanceSoldAt[day-1])
+        return int(max(maxBalanceSoldAt[-1], maxBalanceResetAt[-1]))
