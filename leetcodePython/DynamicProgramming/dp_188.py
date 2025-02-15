1cf66ca540bcf52a61eb3db2315d0f576371d681
Xiang Guo <xiangguoxg@gmail.com>
2025-02-15 14:35:27 -0500

    balance pattern

--- leetcodePython/DynamicProgramming/dp_188.py
+++ leetcodePython/DynamicProgramming/dp_188.py
@@ -1,7 +1,36 @@
 from typing import List
 
 
-class Solution:
+class SolutionMaxBalance:
+    def _maxProfitUnlimitedTransaction(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(0, prices[i] - prices[i-1])
+        return profit
+
+    def maxProfit(self, transaction: int, prices: List[int]) -> int:
+        if not prices or transaction == 0:
+            return 0
+
+        if transaction * 2 >= len(prices):
+            return self._maxProfitUnlimitedTransaction(prices)
+
+        maxBalanceHoldAt = [[-float("inf")] * (transaction+1) for _ in range(len(prices))]
+        maxBalanceSoldAt = [[-float("inf")] * (transaction+1) for _ in range(len(prices))]
+
+        maxBalanceHoldAt[0][1] = -prices[0]
+        maxBalanceSoldAt[0][0] = 0
+
+        for day in range(1, len(prices)):
+            for trans in range(transaction + 1):
+                maxBalanceSoldAt[day][trans] = max(maxBalanceHoldAt[day-1][trans]+prices[day], maxBalanceSoldAt[day-1][trans])
+                if not trans:
+                    continue
+                maxBalanceHoldAt[day][trans] = max(maxBalanceHoldAt[day-1][trans], maxBalanceSoldAt[day-1][trans-1] - prices[day])
+        return int(max(maxBalanceSoldAt[-1][t] for t in range(transaction + 1)))
+
+
+class SolutionMinCostMaxGain:
     def maxProfit(self, transaction: int, prices: List[int]) -> int:
         if transaction == 0:
             return 0
