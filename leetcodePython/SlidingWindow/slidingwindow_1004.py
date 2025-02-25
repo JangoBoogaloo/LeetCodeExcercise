3e2a1f3f00dfed08eba36d894c441a9b7256ef61
Jango <xiangguoxg@gmail.com>
2025-02-25 08:20:40 -0500

    update

--- leetcodePython/SlidingWindow/slidingwindow_1004.py
+++ leetcodePython/SlidingWindow/slidingwindow_1004.py
@@ -3,13 +3,14 @@ from typing import List
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        freq = left = right = 0
+        zero_count = 0
+        left = 0
+
         for right in range(len(nums)):
-            if nums[right] == 1:
-                freq += 1
-            replace = right - left + 1 - freq
-            if replace > k:
-                if nums[left] == 1:
-                    freq -= 1
+            if not nums[right]:
+                zero_count += 1
+            if zero_count > k:
+                if not nums[left]:
+                    zero_count -= 1
                 left += 1
-        return right - left + 1
+        return len(nums) - left
