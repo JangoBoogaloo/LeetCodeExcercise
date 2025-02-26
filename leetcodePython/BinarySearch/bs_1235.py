1e258e8b064f237b6c264d51df31573a32cc5ac7
Xiang Guo <xiangguoxg@gmail.com>
2025-02-26 14:29:15 -0500

    cleanup

--- leetcodePython/BinarySearch/bs_1235.py
+++ leetcodePython/BinarySearch/bs_1235.py
@@ -1,13 +1,14 @@
 from typing import List
 from bisect import bisect_left, bisect
 
-class SolutionDP:
+class Solution:
     def jobScheduling(self, startTimes: List[int], endTimes: List[int], profits: List[int]) -> int:
-        start_end_profit = sorted(zip(startTimes, endTimes, profits), key=lambda job: job[1])
+        end_start_profit = sorted(zip(endTimes, startTimes, profits))
         endTime_maxProfit:List[tuple[int, int]] = [(0, 0)]
-        for start, end, profit in start_end_profit:
-            nextJobIndex = bisect_left(endTime_maxProfit, start+1, key=lambda job: job[0])-1
-            _, nextMaxProfit = endTime_maxProfit[nextJobIndex]
-            if nextMaxProfit + profit > endTime_maxProfit[-1][1]:
-                endTime_maxProfit.append((end, nextMaxProfit+profit))
+        for end, start, profit in end_start_profit:
+            previousJob = bisect_left(endTime_maxProfit, start+1, key=lambda job: job[0])-1
+            _, prevMaxProfit = endTime_maxProfit[previousJob]
+            currentMaxProfit = prevMaxProfit + profit
+            if currentMaxProfit > endTime_maxProfit[-1][1]:
+                endTime_maxProfit.append((end, currentMaxProfit))
         return endTime_maxProfit[-1][1]
