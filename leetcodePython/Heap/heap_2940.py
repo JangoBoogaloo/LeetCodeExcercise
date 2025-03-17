daff6e2055168208bdd1b38dd568d10f0676d932
Xiang Guo <xiangguoxg@gmail.com>
2025-03-17 09:54:23 -0400

    cleanup

--- leetcodePython/Heap/heap_2940.py
+++ leetcodePython/Heap/heap_2940.py
@@ -3,35 +3,35 @@ from typing import List
 
 
 class Solution:
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        answer = [-1] * len(queries)
+        targetHeightAtPosition = self._getQueryTargetHeightAtPositions(heights, queries, answer)
+        minHeight_query = []
+        for pos, high in enumerate(heights):
+            while minHeight_query:
+                minHigh, q_i = minHeight_query[0]
+                if minHigh >= high:
+                    break
+                heappop(minHeight_query)
+                answer[q_i] = pos
+
+            for targetHeight, q_i in targetHeightAtPosition[pos]:
+                heappush(minHeight_query, (targetHeight, q_i))
+        return answer
+
     @staticmethod
-    def _maxPosAndHeightQuery(heights: List[int], queries: List[List[int]], results: List[int]) -> List[List[tuple[int, int]]]:
+    def _getQueryTargetHeightAtPositions(heights: List[int], queries: List[List[int]], answer: List[int]) -> List[List[tuple[int, int]]]:
         maxPosAndHeightQuery = [[] for _ in heights]
         for query_i, query in enumerate(queries):
             posA, posB = query
             if posA < posB and heights[posA] < heights[posB]:
-                results[query_i] = posB
+                answer[query_i] = posB
             elif posA > posB and heights[posA] > heights[posB]:
-                results[query_i] = posA
+                answer[query_i] = posA
             elif posA == posB:
-                results[query_i] = posA
+                answer[query_i] = posA
             else:
                 maxPos = max(posA, posB)
                 maxHeight = max(heights[posA], heights[posB])
                 maxPosAndHeightQuery[maxPos].append((maxHeight, query_i))
         return maxPosAndHeightQuery
-
-    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        results = [-1] * len(queries)
-        maxPosAndHeightQuery = self._maxPosAndHeightQuery(heights, queries, results)
-        minHeightHeap = []
-        for pos, height in enumerate(heights):
-            while minHeightHeap:
-                minHeight, query_i = minHeightHeap[0]
-                if minHeight < height:
-                    heappop(minHeightHeap)
-                    results[query_i] = pos
-                else:
-                    break
-            for h, query_i in maxPosAndHeightQuery[pos]:
-                heappush(minHeightHeap, (h, query_i))
-        return results
