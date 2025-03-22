284dd89ca45dc031a060d93cbac5ce0f9cbadc8e
Xiang Guo <xiangguoxg@gmail.com>
2025-03-22 10:27:35 -0400

    605. Can Place Flowers

---
+++ leetcodePython/Greedy/greedy_605.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        return False
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    flowerbed = [1,0,0,0,1]
+    n = 1
+    print(solution.canPlaceFlowers(flowerbed,n)) 
