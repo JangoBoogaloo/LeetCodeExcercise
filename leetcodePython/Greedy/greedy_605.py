5c4a617cb3e105a536f226cb26a5b1d9d877677b
Xiang Guo <xiangguoxg@gmail.com>
2025-03-22 11:59:59 -0400

    solution

--- leetcodePython/Greedy/greedy_605.py
+++ leetcodePython/Greedy/greedy_605.py
@@ -3,11 +3,16 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        return False
-
-
-if __name__ == "__main__":
-    solution = Solution()
-    flowerbed = [1,0,0,0,1]
-    n = 1
-    print(solution.canPlaceFlowers(flowerbed,n)) 
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 1:
+                continue
+            isLeftEmpty = (i == 0) or flowerbed[i-1] == 0
+            isRightEmpty = (i==len(flowerbed)-1) or flowerbed[i+1] == 0
+            if isLeftEmpty and isRightEmpty:
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return n == 0
