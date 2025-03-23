78b8296df7d7511f3b3051bc133e1a3bfa900e50
Xiang Guo <xiangguoxg@gmail.com>
2025-03-23 14:36:55 -0400

    peak and valley

--- leetcodePython/Greedy/greedy_134.py
+++ leetcodePython/Greedy/greedy_134.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        return 0
+        total = 0
+        start = 0
+        current =0
+        for i in range(len(gas)):
+            balance = gas[i] - cost[i]
+            total += balance
+            current += balance
+            if current < 0:
+                current = 0
+                start = i + 1
+        return start if total >= 0 else -1
