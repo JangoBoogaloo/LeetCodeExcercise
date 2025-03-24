e024cd7873e095ae0ea048c6898e983e7185b6bd
Xiang Guo <xiangguoxg@gmail.com>
2025-03-24 16:22:50 -0400

    cleanup

--- leetcodePython/Tree/tree_428.py
+++ leetcodePython/Tree/tree_428.py
@@ -18,35 +18,38 @@ class Codec:
     def serialize(self, root: Node) -> str:
         if not root:
             return self._NULL
-        data_str_list = []
-        queue = deque()
-        queue.append(root)
-        data_str_list.append(f"{root.val}{self._PARENT_SEPARATOR}{self._NULL}{self._NODE_SEPARATOR}")
-        parent_index = 1
-        while queue:
-            for i in range(len(queue)):
-                parent = queue.popleft()
-                for j in range(len(parent.children)):
-                    queue.append(parent.children[j])
-                    data_str_list.append(
-                        f"{parent.children[j].val}{self._PARENT_SEPARATOR}{parent_index}{self._NODE_SEPARATOR}")
-                parent_index += 1
-        return "".join(data_str_list)[:-1]
+        levelQueue: deque[Node] = deque()
+        nodeStrList = []
+        levelQueue.append(root)
+        nodeStrList.append(self._formatNode(root.val, -1))
+        parentIndex = 1
+        while levelQueue:
+            for i in range(len(levelQueue)):
+                parent = levelQueue.popleft()
+                for child in parent.children:
+                    levelQueue.append(child)
+                    nodeStr = self._formatNode(child.val, parentIndex)
+                    nodeStrList.append(nodeStr)
+                parentIndex += 1
+        return f"{self._NODE_SEPARATOR}".join(nodeStrList)
 
     def deserialize(self, data: str) -> Optional[Node]:
         if data == self._NULL:
             return None
-        node_str_list = data.split(self._NODE_SEPARATOR)
-        node_list = [None]
-        for node_str in node_str_list:
-            value_str, parent_str = node_str.split(self._PARENT_SEPARATOR)
-            parent_pos = 0
-            if parent_str != self._NULL:
-                parent_pos = int(parent_str)
-            val = int(value_str)
-            curr_node = Node(val)
-            node_list.append(curr_node)
-            if not node_list[parent_pos]:
-                continue
-            node_list[parent_pos].children.append(curr_node)
-        return node_list[1]
+        nodeStrList = data.split(self._NODE_SEPARATOR)
+        parentNodes = [None]
+        for nodeStr in nodeStrList:
+            nodeDataStr, parentStr = nodeStr.split(self._PARENT_SEPARATOR)
+            nodeData = int(nodeDataStr)
+            current = Node(nodeData)
+            parentNodes.append(current)
+            if parentStr != self._NULL:
+                parentPos = int(parentStr)
+                parentNodes[parentPos].children.append(current)
+        return parentNodes[1]
+
+    def _formatNode(self, nodeValue: int, parentIndex: int) -> str:
+        parentIndexStr = parentIndex
+        if parentIndex == -1:
+            parentIndexStr = self._NULL
+        return f"{nodeValue}{self._PARENT_SEPARATOR}{parentIndexStr}"
