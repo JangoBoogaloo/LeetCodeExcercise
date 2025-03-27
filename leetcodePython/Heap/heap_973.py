b853b389308ae3a6075dca89f965c05cccb25a16
Xiang Guo <xiangguoxg@gmail.com>
2025-03-27 10:04:21 -0400

    clean solution

--- leetcodePython/Heap/heap_973.py
+++ leetcodePython/Heap/heap_973.py
@@ -1,4 +1,4 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappop, nsmallest
 from typing import List
 
 
@@ -11,3 +11,9 @@ class Solution:
             if len(furthestPoints) > k:
                 heappop(furthestPoints)
         return [point for _, point in furthestPoints]
+
+
+class SolutionNSmallest:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        ans = nsmallest(k, points, key=lambda p: p[0]*p[0]+p[1]*p[1])
+        return ans
