31c244877aa36cb9e136c27c8b791b2ae2de9bd4
Xiang Guo <xiangguoxg@gmail.com>
2025-04-09 19:20:38 -0400

    complete solution

--- leetcodePython/LinkedList/linkedlist_460.py
+++ leetcodePython/LinkedList/linkedlist_460.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 
 class DNode:
-    def __init__(self, value = -1, freq = 1, prev = None, next=None):
+    def __init__(self, key= -1, value=-1, freq=-1, prev=None, nxt=None):
+        self.key = key
         self.value = value
         self.freq = freq
         self.prev = prev
-        self.next = next
+        self.next = nxt
+
 
 class LinkedList:
     def __init__(self):
@@ -15,61 +19,77 @@ class LinkedList:
         self._count = 0
 
     def appendLeft(self, current: DNode):
-        prev = self._dummyHead
-        next = self._dummyHead.next
-        prev.next = current
-        current.prev = prev
-        current.next = next
-        next.prev = current
+        prev, nxt = self._dummyHead, self._dummyHead.next
+        prev.next, current.prev = current, prev
+        nxt.prev, current.next = current, nxt
         self._count += 1
 
-    def removeNode(self, current: DNode) -> bool:
+    def remove(self, current: DNode) -> bool:
         if self._count == 0:
             return False
-        prev = current.prev
-        next = current.next
-        prev.next = next
-        next.prev = prev
+        prev, nxt = current.prev, current.next
+        prev.next, nxt.prev = nxt, prev
         self._count -= 1
         return True
 
-    def removeLast(self) -> bool:
+    def last(self) -> Optional[DNode]:
         if self._count == 0:
-            return False
-        current = self._dummyTail.prev
-        prev = current.prev
-        next = self._dummyTail
-        current.prev, current.next = None, None
-        prev.next = next
-        next.prev = prev
-        return True
+            return None
+        return self._dummyTail.prev
 
+    def count(self) -> int:
+        return self._count
 
 class LFUCache:
-
     def __init__(self, capacity: int):
+        self._freqLinkedListMap = {}
+        self._keyNodeMap = {}
         self._capacity = capacity
-        self._keyValueMap = {}
-        self._freqValueMap = {}
-        self._count = 0
-        self._minAccess = 1
+        self._cacheSize = 0
+        self._minFrequency = 1
 
     def get(self, key: int) -> int:
-        if key not in self._keyValueMap:
+        if key not in self._keyNodeMap:
             return -1
-        valueNode = self._keyValueMap[key]
-        self._updateFrequency(valueNode)
-        return valueNode.value
-
-    def _updateFrequency(self, node: DNode) -> None:
-        frequency = node.freq
-        linklist: LinkedList
-        linklist = self._freqValueMap[node.freq]
-        linklist.removeNode(node)
-        frequency += 1
-        if frequency not in self._freqValueMap:
-            self._freqValueMap[frequency] = LinkedList()
-        self._freqValueMap[frequency].appendLeft(node)
+        node = self._keyNodeMap[key]
+        self._updateFrequency(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        if self._count == self._capacity:
+        if key in self._keyNodeMap:
+            node = self._keyNodeMap[key]
+            node.value = value
+            self._updateFrequency(node)
+            return
+        if self._cacheSize == self._capacity:
+            self._removeLeastFrequentLeastRecent()
+
+        node = DNode(key, value, 1)
+        self._keyNodeMap[key] = node
+        if node.freq not in self._freqLinkedListMap:
+            self._freqLinkedListMap[node.freq] = LinkedList()
+            if node.freq < self._minFrequency:
+                self._minFrequency = node.freq
+        self._freqLinkedListMap[node.freq].appendLeft(node)
+        self._cacheSize += 1
+
+    def _updateFrequency(self, node: DNode) -> None:
+        linkedlist: LinkedList = self._freqLinkedListMap[node.freq]
+        linkedlist.remove(node)
+        if self._minFrequency == node.freq and linkedlist.count() == 0:
+            self._freqLinkedListMap.pop(node.freq)
+            self._minFrequency += 1
+        node.freq += 1
+        if node.freq not in self._freqLinkedListMap:
+            self._freqLinkedListMap[node.freq] = LinkedList()
+        self._freqLinkedListMap[node.freq].appendLeft(node)
+
+    def _removeLeastFrequentLeastRecent(self) -> None:
+        leastFreqList = self._freqLinkedListMap[self._minFrequency]
+        last = leastFreqList.last()
+        if last:
+            leastFreqList.remove(last)
+            self._keyNodeMap.pop(last.key)
+        if leastFreqList.count() == 0:
+            self._freqLinkedListMap.pop(self._minFrequency)
+        self._cacheSize -= 1
