82bdb6ee0bc10f63546842ae3af961523c5c1e98
Xiang Guo <xiangguoxg@gmail.com>
2025-04-12 08:55:47 -0400

    refresh

--- leetcodePython/Sort/sort_1636.py
+++
@@ -1,9 +0,0 @@
-import collections
-from functools import cmp_to_key
-from typing import List
-
-
-class Solution:
-    def frequencySort(self, nums: List[int]) -> List[int]:
-        frequency = collections.Counter(nums)
-        return sorted(nums, key=lambda x: (frequency[x], -x))
