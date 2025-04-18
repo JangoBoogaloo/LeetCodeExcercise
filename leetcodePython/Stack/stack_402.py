5338be6be35b3dcc3f818a2a7a5c670631ad5b90
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 12:39:40 -0400

    [Test] 402. Remove K Digits

--- leetcodePython/Stack/stack_402.py
+++ leetcodePython/Stack/stack_402.py
@@ -13,3 +13,15 @@ class Solution:
 
         numStr = "".join(remainDigits).lstrip('0')
         return numStr if numStr else "0"
+
+
+import pytest
+target = Solution()
+
+@pytest.mark.parametrize("num, k, expected",
+[
+    ("1234", 2, "12"),
+    ("9399", 2, "39")
+])
+def test_removeKdigits(num, k, expected):
+    assert target.removeKdigits(num, k) == expected
