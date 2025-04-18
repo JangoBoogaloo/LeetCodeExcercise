d6b21aa7a057858baee1666e7bc1564801dd0cb8
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 12:57:37 -0400

    more test case

--- leetcodePython/Stack/stack_402.py
+++ leetcodePython/Stack/stack_402.py
@@ -21,7 +21,9 @@ target = Solution()
 @pytest.mark.parametrize("num, k, expected",
 [
     ("1234", 2, "12"),
-    ("9399", 2, "39")
+    ("9399", 2, "39"),
+    ("93929", 2, "329"),
+    ("93929", 3, "29")
 ])
 def test_removeKdigits(num, k, expected):
     assert target.removeKdigits(num, k) == expected
