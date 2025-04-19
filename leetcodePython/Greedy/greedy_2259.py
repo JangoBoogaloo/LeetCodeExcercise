cc90a690ab317b762ad2596b50c668323d180585
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 20:52:37 -0400

    update test case

--- leetcodePython/Greedy/greedy_2259.py
+++ leetcodePython/Greedy/greedy_2259.py
@@ -8,8 +8,13 @@ target = Solution()
 
 @pytest.mark.parametrize("number, digit, expect",
 [
+    ("123", "1", "23"),
     ("1231", "1", "231"),
-    ("9239", "9", "923"),
+    ("6296", "6", "629"),
+    ("6926", "6", "926"),
+    ("6626", "6", "662"),
+    ("53565", "5", "5365"),
+    ("53535", "5", "5353")
 ])
 def test_checkType(number, digit, expect):
     assert target.removeDigit(number, digit) == expect
