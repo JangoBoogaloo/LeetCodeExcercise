6b924fdc84fdc09e0471c5aa8e42aae1622f7229
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 20:11:37 -0400

    [Test] 2259. Remove Digit From Number to Maximize Result

--- leetcodePython/Greedy/greedy_2259.py
+++ leetcodePython/Greedy/greedy_2259.py
@@ -2,3 +2,14 @@ class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
         return ""
 
+
+import pytest
+target = Solution()
+
+@pytest.mark.parametrize("number, digit, expect",
+[
+    ("1231", "1", "231"),
+    ("9239", "9", "923"),
+])
+def test_checkType(number, digit, expect):
+    assert target.removeDigit(number, digit) == expect
