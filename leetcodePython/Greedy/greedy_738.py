e7963cf892e80e76a09927a9c816d0193c97057e
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 22:22:56 -0400

    cases

--- leetcodePython/Greedy/greedy_738.py
+++ leetcodePython/Greedy/greedy_738.py
@@ -8,6 +8,10 @@ target = Solution()
 @pytest.mark.parametrize("n, expect",
 [
     (10, 9),
+    (333, 333),
+    (332, 299),
+    (343, 339),
+    (2332, 2299)
 ])
 def test_monotoneIncreasingDigits(n, expect):
     assert target.monotoneIncreasingDigits(n) == expect
