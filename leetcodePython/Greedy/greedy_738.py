73f23b15cdf2d0f823658fe62d8b4db01203b069
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 22:22:26 -0400

    Solution

--- leetcodePython/Greedy/greedy_738.py
+++ leetcodePython/Greedy/greedy_738.py
@@ -1,5 +1,22 @@
 class Solution:
     def monotoneIncreasingDigits(self, n: int) -> int:
-        return -1
-
+        digits = list(str(n))
+        biggerDigitIndex = 0
+        smallerExist = False
+        i = 0
+        for i in range(1, len(digits)):
+            if digits[i-1] < digits[i]:
+                biggerDigitIndex = i
+            elif digits[i-1] == digits[i]:
+                continue
+            else:
+                smallerExist = True
+                break
+        if not smallerExist:
+            return n
+        digits[biggerDigitIndex] = str(int(digits[biggerDigitIndex]) - 1)
+        remain9 = ["9"]*(len(digits)-1-biggerDigitIndex)
+        digits = digits[:biggerDigitIndex+1] + remain9
+        numStr = "".join(digits).lstrip("0")
+        return int(numStr)
 
