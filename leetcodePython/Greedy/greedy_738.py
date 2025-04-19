327509c0a8dd93d64b523c797e743ecfb9898b28
Xiang Guo <xiangguoxg@gmail.com>
2025-04-18 21:41:47 -0400

    [Test] 738. Monotone Increasing Digits

--- leetcodePython/Greedy/greedy_738.py
+++ leetcodePython/Greedy/greedy_738.py
@@ -2,3 +2,12 @@ class Solution:
     def monotoneIncreasingDigits(self, n: int) -> int:
 
 
+import pytest
+target = Solution()
+
+@pytest.mark.parametrize("n, expect",
+[
+    (10, 9),
+])
+def test_monotoneIncreasingDigits(n, expect):
+    assert target.monotoneIncreasingDigits(n) == expect
