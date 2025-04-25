3a944e7a298b3ad2362c929d4a711dc7e3e28de4
Xiang Guo <xiangguoxg@gmail.com>
2025-04-25 17:06:18 -0400

    Add test cases

--- leetcodePython/Stack/stack_321.py
+++ leetcodePython/Stack/stack_321.py
@@ -13,7 +13,11 @@ target = Solution()
 
 @pytest.mark.parametrize("nums1, nums2, k, expect",
 [
-    ([], [], -1, []),
+    ([1, 2, 3], [1, 3], 2, [3, 3]),
+    ([1, 2, 3], [1, 3], 3, [2, 3, 3]),
+    ([3, 1, 2], [1, 3], 2, [3, 3]),
+    ([3, 1, 2], [1, 3], 3, [3, 3, 2]),
+    ([3, 1, 2], [1], 3, [3, 2, 1])
 ])
 def test_checkType(nums1, nums2, k, expect):
     assert target.maxNumber(nums1, nums2, k) == expect
