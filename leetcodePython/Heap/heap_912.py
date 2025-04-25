4916aa71ffccd9cba59825d3100a9abf85407088
Xiang Guo <xiangguoxg@gmail.com>
2025-04-25 14:20:10 -0400

    rebase

--- leetcodePython/Heap/heap_912.py
+++ leetcodePython/Heap/heap_912.py
@@ -2,31 +2,26 @@ from typing import List
 
 
 class Solution:
-    def _heapify(self, nums: List[int], root_index: int, end_index: int):
-        left_child_index = 2 * root_index + 1
-        right_child_index = 2 * root_index + 2
+    def _heapifyMaxHeap(self, nums: List[int], rootIndex: int, indexLimit: int):
+        leftIndex = 2 * rootIndex + 1
+        rightIndex = 2 * rootIndex + 2
 
-        largest_index = root_index
-        if left_child_index < end_index and nums[largest_index] < nums[left_child_index]:
-            largest_index = left_child_index
-        if right_child_index < end_index and nums[largest_index] < nums[right_child_index]:
-            largest_index = right_child_index
+        largestIndex = rootIndex
+        if leftIndex < indexLimit and nums[largestIndex] < nums[leftIndex]:
+            largestIndex = leftIndex
+        if rightIndex < indexLimit and nums[largestIndex] < nums[rightIndex]:
+            largestIndex = rightIndex
 
-        if largest_index != root_index:
-            nums[largest_index], nums[root_index] = nums[root_index], nums[largest_index]
-            self._heapify(nums, largest_index, end_index)
+        if largestIndex != rootIndex:
+            nums[largestIndex], nums[rootIndex] = nums[rootIndex], nums[largestIndex]
+            self._heapifyMaxHeap(nums, largestIndex, indexLimit)
 
     def sortArray(self, nums: List[int]) -> List[int]:
         length = len(nums)
         for i in range(length // 2 + 1)[::-1]:
-            self._heapify(nums, i, length)
+            self._heapifyMaxHeap(nums, i, length)
 
         for i in range(length)[::-1]:
             nums[i], nums[0] = nums[0], nums[i]
-            self._heapify(nums, 0, i)
+            self._heapifyMaxHeap(nums, 0, i)
         return nums
-
-
-if __name__ == "__main__":
-    sol = Solution()
-    sol.sortArray([5,4,3,2,7])
