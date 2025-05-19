9a3f075b45c38e131888053c37f0bbd7ad15a26d
Xiang Guo <xiangguoxg@gmail.com>
2025-05-19 11:56:59 -0400

    another solution

---
+++ leetcodePython/TwoPointers/twopointers_350_nosort.py
@@ -0,0 +1,22 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            return self.intersect(nums2, nums1)
+
+        counter1 = Counter(nums1)
+        intersect = []
+        for num in nums2:
+            if counter1[num] > 0:
+                counter1[num] -= 1
+                intersect.append(num)
+        return intersect
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    ans = sol.intersect([4,9,5], [9,4,9,8,4])
+    print(ans)
