48305ae490920924ed4eb056656d06c040fcde98
Xiang Guo <xiangguoxg@gmail.com>
2025-05-21 09:05:57 -0400

    280. Wiggle Sort

---
+++ leetcodePython/Sort/sort_280.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        nums.sort()
+        for i in range(1, len(nums)-1, 2):
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+
+
+
