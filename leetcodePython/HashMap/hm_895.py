d8be5196f8e90c87fe5d1b5d75595e4a87a5798c
Xiang Guo <xiangguoxg@gmail.com>
2025-05-31 13:32:59 -0400

    895. Maximum Frequency Stack

---
+++ leetcodePython/HashMap/hm_895.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+class FreqStack:
+
+    def __init__(self):
+        self._freqOf = defaultdict(int)
+        self._stackOf = defaultdict(list)
+
+    def push(self, val: int) -> None:
+        self._freqOf[val] += 1
+        self._stackOf[self._freqOf[val]].append(val)
+
+    def pop(self) -> int:
+        maxFreq = len(self._stackOf)
+        value = self._stackOf[maxFreq].pop()
+        self._freqOf[value] -= 1
+        if not self._stackOf[maxFreq]:
+            del self._stackOf[maxFreq]
+        return value
