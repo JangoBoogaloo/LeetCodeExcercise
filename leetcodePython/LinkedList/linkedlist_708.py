bf0d329b72c3afe62080f282546c3d8b1fa6f1a6
Xiang Guo <xiangguoxg@gmail.com>
2025-06-06 23:13:47 -0400

    solution

---
+++ leetcodePython/LinkedList/linkedlist_708.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from LinkedList.linkedlist import CircularList
+from linkedlist import Node
+
+
+class Solution:
+    def insert(self, head: Optional[Node], insertVal: int) -> Node:
+        if not head:
+            head = Node(insertVal)
+            head.next = head
+            return head
+        current = head
+        while current.next != head:
+            if current.val <= current.next.val:
+                # curr < insert < next
+                if current.val <= insertVal <= current.next.val:
+                    break
+            else:
+                if current.val <= insertVal:
+                    break
+                if current.next.val >= insertVal:
+                    break
+            current = current.next
+        newNode = Node(insertVal)
+        current.next, newNode.next = newNode, current.next
+        return head
+
+
+
+
