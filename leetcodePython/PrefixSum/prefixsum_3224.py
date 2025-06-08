57a6359c9eb6f414b77389add2db4d0d73ad23d7
Xiang Guo <xiangguoxg@gmail.com>
2025-06-08 15:31:54 -0400

    3224. Minimum Array Changes to Make Differences Equal

--- leetcodePython/PrefixSum/prefixsum_3224.py
+++ leetcodePython/PrefixSum/prefixsum_3224.py
@@ -3,4 +3,20 @@ from typing import List
 
 class Solution:
     def minChanges(self, nums: List[int], k: int) -> int:
-        return -1
+        changesAtDiff = [0] * (k + 1 + 1)
+        changesAtDiff[0] = len(nums) // 2
+        print(changesAtDiff)
+        for i in range(len(nums) // 2):
+            leftNum, rightNum = nums[i], nums[len(nums) - 1 - i]
+            diff = abs(leftNum - rightNum)
+            maxDiff = max(leftNum, rightNum, k-leftNum, k-rightNum)
+            changesAtDiff[diff] -= 1
+            changesAtDiff[diff + 1] += 1
+            changesAtDiff[maxDiff + 1] += 1
+
+        changes = 0
+        minChange = len(nums) // 2
+        for diff in range(k+1):
+            changes += changesAtDiff[diff]
+            minChange = min(minChange, changes)
+        return minChange
