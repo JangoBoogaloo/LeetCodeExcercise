9947b791b91063617db505f5bf18c653dd80c9ad
Xiang Guo <xiangguoxg@gmail.com>
2025-06-16 09:20:11 -0400

    greedy

--- leetcodePython/Greedy/greedy_1526.py
+++ leetcodePython/Greedy/greedy_1526.py
@@ -9,17 +9,9 @@ class Solution:
         return ops
 
 
-class SolutionMonotonicStack:
-    def minNumberOperations(self, target: List[int]) -> int:
-        operations = 0
-        decreaseHeightAndOps = [(0, 0)]
-        for height in target:
-            ops = max(0, height - decreaseHeightAndOps[-1][0])
-            while decreaseHeightAndOps and height > decreaseHeightAndOps[-1][0]:
-                _, prevOps = decreaseHeightAndOps.pop()
-                operations += prevOps
-            decreaseHeightAndOps.append((height, ops))
 
-        while decreaseHeightAndOps:
-            operations += decreaseHeightAndOps.pop()[1]
-        return operations
+
+
+
+
+
