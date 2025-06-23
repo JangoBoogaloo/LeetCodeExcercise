298b051f73679514b8fc58e83d376fcb8d9323f6
Xiang Guo <xiangguoxg@gmail.com>
2025-06-23 08:43:19 -0400

    start

---
+++ leetcodePython/BinarySearch/bs_362.py
@@ -0,0 +1,9 @@
+class HitCounter:
+    def __init__(self):
+        return
+
+    def hit(self, timestamp: int) -> None:
+        return
+
+    def getHits(self, timestamp: int) -> int:
+        return -1
