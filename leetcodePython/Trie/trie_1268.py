389dc16ffbaf2f5cc0346fd017c3e0506c777377
Xiang Guo <xiangguoxg@gmail.com>
2025-06-29 16:18:40 -0400

    optimization

--- leetcodePython/Trie/trie_1268.py
+++ leetcodePython/Trie/trie_1268.py
@@ -4,11 +4,12 @@ from trie_impl import Trie
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         trie = Trie()
+        products.sort()
         for product in products:
             trie.insert(product)
         answer = []
         for i in range(1, len(searchWord)+1):
-            answer.append(sorted(trie.wordsWithPrefix(searchWord[:i]))[:3])
+            answer.append(trie.wordsWithPrefix(searchWord[:i])[:3])
         return answer
 
 
