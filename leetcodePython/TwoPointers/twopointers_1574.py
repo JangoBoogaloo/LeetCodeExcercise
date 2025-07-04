f1bb9d97ec5cf7b3e9bf239814219f6795b54b1a
Xiang Guo <xiangguoxg@gmail.com>
2025-07-04 09:21:56 -0400

    add more approaches

--- leetcodePython/TwoPointers/twopointers_1574.py
+++ leetcodePython/TwoPointers/twopointers_1574.py
@@ -3,14 +3,24 @@ from typing import List
 
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        right = len(arr) - 1
-        while right > 0 and arr[right] >= arr[right - 1]:
-            right -= 1
-        removeSubarrayLength = right
+        rightBoundary = len(arr) - 1
+        while rightBoundary > 0 and arr[rightBoundary] >= arr[rightBoundary - 1]:
+            rightBoundary -= 1
+        if rightBoundary == 0:
+            return 0
+        removeSubarrayLength = rightBoundary
         left = 0
-        while left < right and (left == 0 or arr[left-1] <= arr[left]):
-            while right < len(arr) and arr[left] > arr[right]:
-                right += 1
-            removeSubarrayLength = min(removeSubarrayLength, (right - 1) - (left + 1) + 1)
+        while left < rightBoundary:
+            while rightBoundary < len(arr) and arr[left] > arr[rightBoundary]:
+                rightBoundary += 1
+            removeSubarrayLength = min(removeSubarrayLength, (rightBoundary - 1) - (left + 1) + 1)
+            if arr[left] > arr[left + 1]:
+                break
             left += 1
         return removeSubarrayLength
+
+
+
+
+
+
