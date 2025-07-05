939a38a3e91a1552349cca985f94d4d7cbb1fbb9
Xiang Guo <xiangguoxg@gmail.com>
2025-07-05 13:03:30 -0400

    rebase

--- leetcodePython/TwoPointers/twopointers_75.py
+++ leetcodePython/TwoPointers/twopointers_75.py
@@ -1,38 +1,23 @@
 from typing import List
 
 
-class SolutionCountSort:
+class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        count_0, count_1, count_2 = 0, 0, 0
-        for num in nums:
-            if num == 0:
-                count_0 += 1
-            elif num == 1:
-                count_1 += 1
+        indexZero = 0
+        indexTwo = len(nums) - 1
+        currentIndex = 0
+        while currentIndex <= indexTwo:
+            if nums[currentIndex] == 0:
+                nums[indexZero], nums[currentIndex] = nums[currentIndex], nums[indexZero]
+                indexZero += 1
+                currentIndex += 1
+            elif nums[currentIndex] == 2:
+                nums[indexTwo], nums[currentIndex] = nums[currentIndex], nums[indexTwo]
+                indexTwo -= 1
             else:
-                count_2 += 1
-        for i in range(len(nums)):
-            if count_0 > 0:
-                nums[i] = 0
-                count_0 -= 1
-            elif count_1 > 0:
-                nums[i] = 1
-                count_1 -= 1
-            else:
-                nums[i] = 2
+                currentIndex += 1
 
 
-class SolutionDutchNationalFlag:
-    def sortColors(self, nums: List[int]) -> None:
-        i1, i2, i3 = 0, 0, len(nums) - 1
-        num1, num2, num3 = 0, 1, 2
-        while i2 <= i3:
-            if nums[i2] == num1:
-                nums[i1], nums[i2] = nums[i2], nums[i1]
-                i1 += 1
-                i2 += 1
-            elif nums[i2] == num2:
-                i2 += 1
-            else:  # nums[i2] == num3
-                nums[i2], nums[i3] = nums[i3], nums[i2]
-                i3 -= 1
+
+
+
