868edd501251039ce249a84253a4be6434e8f9e4
Jango <1152134+JangoBoogaloo@users.noreply.github.com>
2025-07-06 15:28:11 -0400

    2076. Process Restricted Friend Requests (#562)

---
+++ leetcodePython/Graph/graph_2076.py
@@ -0,0 +1,27 @@
+from typing import List
+from union_find import UF
+
+class Solution:
+    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
+        uf = UF()
+        [uf.add(n) for i in range(n)]
+        answer = []
+        for a, b in requests:
+            componentA, componentB = uf.find(a), uf.find(b)
+            for c, d in restrictions:
+                componentC, componentD = uf.find(c), uf.find(d)
+                if (componentA == componentC and componentB == componentD) or (componentA == componentD and componentB == componentC):
+                    answer.append(False)
+                    break
+            else:
+                answer.append(True)
+                uf.union(componentA, componentB)
+        return answer
+
+
+
+
+
+
+
+
