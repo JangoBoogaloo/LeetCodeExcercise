330a43d62f7223e5a4c6c0658fce86ef3519da34
Xiang Guo <xiangguoxg@gmail.com>
2025-07-07 09:34:48 -0400

    [Test] 131. Palindrome Partitioning

---
+++ leetcodePython/Test/test_131.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import pytest
+
+import Backtracking.bt_131, DynamicProgramming.dp_131
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        return []
+
+
+testCases = [
+    ("a", [["a"]]),
+    ("aa", [["a", "a"], ["aa"]]),
+    ("aaa", [["a", "a", "a"], ["a", "aa"], ["aa", "a"], ["aaa"]]),
+]
+@pytest.mark.parametrize("txt, expect", testCases)
+def test_131_partion(txt, expect):
+    def run131Test(target):
+        target = Backtracking.bt_131.Solution()
+        actual = target.partition(txt)
+        for a in actual:
+            assert a in expect
+        for e in expect:
+            assert e in actual
+    run131Test(Backtracking.bt_131.Solution())
+    run131Test(DynamicProgramming.dp_131.Solution())
