515fe0f09fa005b186be05a30efc304dd2a7b02c
Xiang Guo <xiangguoxg@gmail.com>
2025-07-07 09:45:27 -0400

    fix

--- leetcodePython/Test/test_131.py
+++ leetcodePython/Test/test_131.py
@@ -1,13 +1,15 @@
 from typing import List
 import Backtracking.bt_131, DynamicProgramming.dp_131
-
-import pytest
-
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         return []
 
 
+
+
+import pytest
+
+target = DynamicProgramming.dp_131.Solution()
 testCases = [
     ("a", [["a"]]),
     ("aa", [["a", "a"], ["aa"]]),
@@ -15,12 +17,8 @@ testCases = [
 ]
 @pytest.mark.parametrize("txt, expect", testCases)
 def test_131_partion(txt, expect):
-    def run131Test(target):
-        target = Backtracking.bt_131.Solution()
-        actual = target.partition(txt)
-        for a in actual:
-            assert a in expect
-        for e in expect:
-            assert e in actual
-    run131Test(Backtracking.bt_131.Solution())
-    run131Test(DynamicProgramming.dp_131.Solution())
+    actual = target.partition(txt)
+    for a in actual:
+        assert a in expect
+    for e in expect:
+        assert e in actual
