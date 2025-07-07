51cff78a882108e10abc493497141d005829ff01
Xiang Guo <xiangguoxg@gmail.com>
2025-07-07 09:26:19 -0400

    solutions

--- leetcodePython/Backtracking/bt_131.py
+++ leetcodePython/Backtracking/bt_131.py
@@ -2,51 +2,27 @@ from typing import List
 
 
 class Solution:
-    @staticmethod
-    def _isPalindrome(s: str) -> bool:
-        left, right = 0, len(s)-1
+    def _isPalindrome(self, txt: str) -> bool:
+        left, right = 0, len(txt) - 1
         while left < right:
-            if s[left] != s[right]:
+            if txt[left] != txt[right]:
                 return False
             left += 1
             right -= 1
         return True
 
     def partition(self, s: str) -> List[List[str]]:
-        allPossible = []
-
-        def backtrack(start: int, palindromes: List[str]) -> None:
-            if start == len(s):
-                allPossible.append(list(palindromes))
+        allPartitions = []
+        def backtrack(startIndex: int, currentPalindromes: List[str]):
+            if startIndex == len(s):
+                allPartitions.append(list(currentPalindromes))
                 return
-            for end in range(start+1, len(s)+1):
-                sub_str = s[start:end]
-                if not self._isPalindrome(sub_str):
+            for end in range(startIndex+1, len(s)+1):
+                subStr = s[startIndex:end]
+                if not self._isPalindrome(subStr):
                     continue
-                palindromes.append(sub_str)
-                backtrack(end, palindromes)
-                palindromes.pop()
-
+                currentPalindromes.append(subStr)
+                backtrack(end, currentPalindromes)
+                currentPalindromes.pop()
         backtrack(0, [])
-        return allPossible
-
-
-class SolutionDP:
-
-    def partition(self, s: str) -> List[List[str]]:
-        allPossible = []
-        dp = [[False] * len(s) for _ in range(len(s))]
-
-        def backtrack(start: int, palindromes: List[str]) -> None:
-            if start == len(s):
-                allPossible.append(list(palindromes))
-                return
-            for end in range(start, len(s)):
-                if s[start] == s[end] and (end - start <= 2 or dp[start + 1][end-1]):
-                    dp[start][end] = True
-                    palindromes.append(s[start:end+1])
-                    backtrack(end+1, palindromes)
-                    palindromes.pop()
-
-        backtrack(0, [])
-        return allPossible
+        return allPartitions
---
+++ leetcodePython/DynamicProgramming/dp_131.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        allPartitions = []
+        isPalindrome = defaultdict(bool)
+        def backtrack(startIndex: int, currentPalindromes: List[str]):
+            if startIndex == len(s):
+                allPartitions.append(list(currentPalindromes))
+                return
+            for end in range(startIndex, len(s)):
+                if s[startIndex] == s[end] and (end - startIndex <= 2 or isPalindrome[(startIndex + 1, end-1)]):
+                    isPalindrome[(startIndex, end)] = True
+                    currentPalindromes.append(s[startIndex: end+1])
+                    backtrack(end+1, currentPalindromes)
+                    currentPalindromes.pop()
+
+        backtrack(0, [])
+        return allPartitions
