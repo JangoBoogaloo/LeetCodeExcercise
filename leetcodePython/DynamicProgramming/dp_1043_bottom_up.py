74eec4d2655225705b1be48185d4abc1cae00768
Xiang Guo <xiangguoxg@gmail.com>
2025-07-27 09:44:08 -0400

    1043. Partition Array for Maximum Sum

---
+++ leetcodePython/DynamicProgramming/dp_1043_bottom_up.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        maxSumEndAt = [0] * (len(arr) + 1)
+        for end in range(1, len(maxSumEndAt)):
+            currMax = 0
+            for size in range(1, min(k, end)+1):
+                currMax = max(currMax, arr[end-size])
+                maxSumEndAt[end] = max(maxSumEndAt[end], maxSumEndAt[end-size] + currMax * size)
+        return maxSumEndAt[-1]
