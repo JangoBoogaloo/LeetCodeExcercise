ad3288ff3591597360819913c9f0db86c7f82fa6
Xiang Guo <xiangguoxg@gmail.com>
2025-08-04 15:22:31 -0400

    better syntax

--- leetcodePython/DynamicProgramming/dp_1312.py
+++ leetcodePython/DynamicProgramming/dp_1312.py
@@ -3,7 +3,7 @@ class Solution:
         longestCommonEndAt = [[0] * (len(s) + 1) for _ in range(len(s) + 1)]
         for i1 in range(1, len(s)+1):
             for i2 in range(1, len(s)+1):
-                if s[i1-1] == s[len(s) - i2]:
+                if s[i1-1] == s[-i2]:
                     longestCommonEndAt[i1][i2] = 1 + longestCommonEndAt[i1-1][i2-1]
                 else:
                     longestCommonEndAt[i1][i2] = max(longestCommonEndAt[i1-1][i2], longestCommonEndAt[i1][i2-1])
