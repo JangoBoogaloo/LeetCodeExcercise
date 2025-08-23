67a853e999969d6f202344177e9b99016b0e2252
Xiang Guo <xiangguoxg@gmail.com>
2025-08-23 10:21:49 -0400

    cleanup

--- leetcodePython/DynamicProgramming/dp_983.py
+++ leetcodePython/DynamicProgramming/dp_983.py
@@ -2,23 +2,25 @@ from typing import List
 
 
 class Solution:
-    def _costForPassFrom(self, day: int, cost: int, costAt: List[int]) -> int:
-        beforePassDay = max(0, day)
-        return costAt[beforePassDay] + cost
-
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         lastDay = days[-1]
         costAt = [0] * (lastDay + 1)
-        dayIndex = 0
-        passCostFor = {1: costs[0], 7: costs[1], 30:costs[2]}
-
-        for day in range(1, lastDay + 1):
-            if day < days[dayIndex]:
+        ticketForDays = {1: costs[0], 7: costs[1], 30: costs[2]}
+        playDayIndex = 0
+        for day in range(1, lastDay+1):
+            if day < days[playDayIndex]:
                 costAt[day] = costAt[day-1]
                 continue
-            dayIndex += 1
+            playDayIndex += 1
             costAt[day] = float("inf")
-            for dayOption in passCostFor:
-                passStartDay = day - dayOption
-                costAt[day] = min(costAt[day], self._costForPassFrom(passStartDay, passCostFor[dayOption], costAt))
+            for ticketDays in ticketForDays:
+                ticketStartDay = max(0, day - ticketDays)
+                costAt[day] = min(costAt[day], costAt[ticketStartDay] + ticketForDays[ticketDays])
         return costAt[-1]
+
+
+
+
+
+
+
