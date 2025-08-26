d4ff16b3688f78cc7f7b081a80ebf19d90d3d60d
Xiang Guo <xiangguoxg@gmail.com>
2025-08-25 21:05:34 -0400

    topdown

--- leetcodePython/DynamicProgramming/dp_983_top_down.py
+++ leetcodePython/DynamicProgramming/dp_983_top_down.py
@@ -2,16 +2,28 @@ from typing import List
 
 
 class Solution:
-    def _minCostAt(self, day: int, days: set[int], costForDays: dict[int, int]) -> int:
-        minCost = float("inf")
+    def _minCostFrom(self, day: int, lastDay:int, days: set[int], costForDays: dict[int, int], minCostAt: List[int]) -> int:
+        if day > lastDay:
+            return 0
         if day not in days:
-            return self._minCostAt(day-1, days, costForDays)
-        for ticketEffectDays in costForDays:
-            ticketStartDay = max(0, day - ticketEffectDays)
-            minCost = min(minCost, self._minCostAt(ticketStartDay, days, costForDays))
-        return minCost
+            return self._minCostFrom(day+1, lastDay, days, costForDays, minCostAt)
+
+        if minCostAt[day] != -1:
+            return minCostAt[day]
+        minCost = float('inf')
+        for ticketDays in costForDays:
+            minCost = min(minCost, costForDays[ticketDays] + self._minCostFrom(day + ticketDays, lastDay, days, costForDays, minCostAt))
+        minCostAt[day] = minCost
+        return minCostAt[day]
 
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         costForDays = {1: costs[0], 7: costs[1], 30: costs[2]}
+        lastDay = days[-1]
         days = set(days)
-        return self._minCostAt(days[-1], days, costForDays)
+        minCostAt = [-1] * (lastDay + 1)
+        return self._minCostFrom(1, lastDay, days, costForDays, minCostAt)
+
+
+
+
+
