6dc686099cfb9b341f972363bf1e70ae99701933
Xiang Guo <xiangguoxg@gmail.com>
2025-08-27 13:34:50 -0400

    198. House Robber

---
+++ leetcodePython/DynamicProgramming/dp_198/dp_198_top_down.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def _robGainStartAt(self, index, robGainStartAt, nums) -> int:
+        if index >= len(nums):
+            return 0
+        if robGainStartAt[index] != -1:
+            return robGainStartAt[index]
+        robCurrent = nums[index] + self._robGainStartAt(index+2, robGainStartAt, nums)
+        skipCurrent = self._robGainStartAt(index + 1, robGainStartAt, nums)
+        robGainStartAt[index] = max(robCurrent, skipCurrent)
+        return robGainStartAt[index]
+
+    def rob(self, nums: List[int]) -> int:
+        robGainStartAt = [-1] * len(nums)
+        return self._robGainStartAt(0, robGainStartAt, nums)
+        # rob[x] = rob[x-1] or rob[x] = rob[x-2] + num[x]
