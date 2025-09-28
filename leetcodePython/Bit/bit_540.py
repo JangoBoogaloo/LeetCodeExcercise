07ea160b92f229f66aa1a83f0d75d8bb7ae004ce
Xiang Guo <xiangguoxg@gmail.com>
2025-09-28 10:57:13 -0400

    solution

---
+++ leetcodePython/Bit/bit_540.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        res=0
+        for num in nums:
+            res^=num
+        return res
