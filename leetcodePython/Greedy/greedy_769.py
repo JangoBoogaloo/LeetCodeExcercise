92b03217aae2294a5ff60d2116b8ee393ea4072d
Xiang Guo <xiangguoxg@gmail.com>
2025-11-03 12:08:23 -0500

    result

--- leetcodePython/Greedy/greedy_769.py
+++ leetcodePython/Greedy/greedy_769.py
@@ -1,37 +1,19 @@
 from typing import List
 
 
-class SolutionMonotonicStack:
-    def maxChunksToSorted(self, arr: List[int]) -> int:
-        increaseStack = []
-        for num in arr:
-            largest = num
-            while increaseStack and num < increaseStack[-1]:
-                largest = max(largest, increaseStack.pop())
-            increaseStack.append(largest)
-        return len(increaseStack)
-
-
-class SolutionMaxElement:
-    def maxChunksToSorted(self, arr: List[int]) -> int:
-        sortedCurrentSum = 0
-        currSum = 0
-        chunks = 0
-        for i in range(len(arr)):
-            sortedCurrentSum += i
-            currSum += arr[i]
-            if currSum == sortedCurrentSum:
-                chunks += 1
-        return chunks
-
-
-class SolutionPrefixSum:
+class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         currMax = 0
-        chunks = 0
-        for i in range(len(arr)):
-            currMax = max(currMax, arr[i])
-            max_i = i
-            if currMax == max_i:
-                chunks += 1
-        return chunks
+        matchMaxCount = 0
+        for i, num in enumerate(arr):
+            currMax = max(currMax, num)
+            expectedMax = i
+            if currMax == expectedMax:
+                matchMaxCount += 1
+        return matchMaxCount
+
+
+
+
+
+
