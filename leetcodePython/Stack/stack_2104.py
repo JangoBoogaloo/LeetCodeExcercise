3b826ab9602e0c4ab679ef3f973e27e8773b5cd4
Xiang Guo <xiangguoxg@gmail.com>
2025-11-05 18:57:44 -0500

    reviewed

--- leetcodePython/Stack/stack_2104.py
+++ leetcodePython/Stack/stack_2104.py
@@ -2,47 +2,37 @@ from typing import List
 
 
 class Solution:
-
     @staticmethod
     def _minSum(nums: List[int]) -> int:
         increaseIndexStack = []
-        length = len(nums)
         minSum = 0
-        for right in range(length + 1):
-            while increaseIndexStack and (right == length or nums[right] <= nums[increaseIndexStack[-1]]):
-                minMidIndex = increaseIndexStack.pop()
-                left = -1 if not increaseIndexStack else increaseIndexStack[-1]
-                minUsageCount = (right - minMidIndex) * (minMidIndex - left)
-                minSum += nums[minMidIndex] * minUsageCount
+        nums.append(float('-inf'))
+        for right in range(len(nums)):
+            currNum = nums[right]
+            while increaseIndexStack and currNum <= nums[increaseIndexStack[-1]]:
+                minIndex = increaseIndexStack.pop()
+                lessThanMinIndex = -1 if not increaseIndexStack else increaseIndexStack[-1]
+                minUsageCount = (right - minIndex) * (minIndex - lessThanMinIndex)
+                minSum += nums[minIndex] * minUsageCount
             increaseIndexStack.append(right)
+        nums.pop()
         return minSum
 
     @staticmethod
     def _maxSum(nums: List[int]) -> int:
         decreaseIndexStack = []
-        length = len(nums)
         maxSum = 0
-        for right in range(length + 1):
-            while decreaseIndexStack and (right == length or nums[right] >= nums[decreaseIndexStack[-1]]):
-                maxMidIndex = decreaseIndexStack.pop()
-                left = -1 if not decreaseIndexStack else decreaseIndexStack[-1]
-                maxUsageCount = (right - maxMidIndex) * (maxMidIndex - left)
-                maxSum += nums[maxMidIndex] * maxUsageCount
+        nums.append(float('inf'))
+        for right in range(len(nums)):
+            currNum = nums[right]
+            while decreaseIndexStack and currNum >= nums[decreaseIndexStack[-1]]:
+                maxIndex = decreaseIndexStack.pop()
+                moreThanMaxIndex = -1 if not decreaseIndexStack else decreaseIndexStack[-1]
+                maxUsageCount = (right - maxIndex) * (maxIndex - moreThanMaxIndex)
+                maxSum += nums[maxIndex] * maxUsageCount
             decreaseIndexStack.append(right)
+        nums.pop()
         return maxSum
 
     def subArrayRanges(self, nums: List[int]) -> int:
         return self._maxSum(nums) - self._minSum(nums)
-
-
-class SolutionBruteForce:
-    @staticmethod
-    def _getRange(nums: List[int]) -> int:
-        return max(nums) - min(nums)
-
-    def subArrayRanges(self, nums: List[int]) -> int:
-        sumRange = 0
-        for end in range(len(nums)):
-            for start in range(end+1):
-                sumRange += self._getRange(nums[start:end+1])
-        return sumRange
